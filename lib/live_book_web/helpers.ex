9f83cddb9a46bfa445d534ab1e2381a9ca31bc28
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-02-17 16:26:28 +0100

    Show appropriate shortcuts depending on the user system

--- lib/live_book_web/helpers.ex
+++ lib/live_book_web/helpers.ex
@@ -12,4 +12,12 @@ defmodule LiveBookWeb.Helpers do
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
     live_component(socket, LiveBookWeb.ModalComponent, modal_opts)
   end
+
+  @doc """
+  Determines if the request comes from a Mac user based on the *User-Agent* header.
+  """
+  def mac_user?(conn) do
+    [user_agent] = Plug.Conn.get_req_header(conn, "user-agent")
+    String.match?(user_agent, ~r/Mac OS X/)
+  end
 end
--- lib/live_book_web/live/shortcuts_component.ex
+++ lib/live_book_web/live/shortcuts_component.ex
@@ -2,16 +2,56 @@ defmodule LiveBookWeb.ShortcutsComponent do
   use LiveBookWeb, :live_component
 
   @shortcuts [
-    %{combination: "?", description: "Open this help modal"},
-    %{combination: "cmd + enter", description: "Evaluate Elixir cell / Toggle Markdown cell"},
-    %{combination: "shift + enter", description: "Evaluate cell and focus next"},
-    %{combination: "cmd + shift + enter", description: "Evaluate section"},
-    %{combination: "cmd + opt + enter", description: "Evaluate cells below"},
-    %{combination: "cmd + j", description: "Focus next cell"},
-    %{combination: "cmd + k", description: "Focus previous cell"},
-    %{combination: "cmd + opt + n", description: "Insert Elixir cell below"},
-    %{combination: "cmd + opt + m", description: "Insert Markdown cell below"},
-    %{combination: "cmd + opt + w", description: "Delete cell"}
+    %{
+      combo: "?",
+      mac_combo: "?",
+      description: "Open this help modal"
+    },
+    %{
+      combo: "ctrl + enter",
+      mac_combo: "cmd + enter",
+      description: "Evaluate Elixir cell / Toggle Markdown cell"
+    },
+    %{
+      combo: "shift + enter",
+      mac_combo: "shift + enter",
+      description: "Evaluate cell and focus next"
+    },
+    %{
+      combo: "ctrl + shift + enter",
+      mac_combo: "cmd + shift + enter",
+      description: "Evaluate section"
+    },
+    %{
+      combo: "ctrl + alt + enter",
+      mac_combo: "cmd + opt + enter",
+      description: "Evaluate cells below"
+    },
+    %{
+      combo: "ctrl + j",
+      mac_combo: "cmd + j",
+      description: "Focus next cell"
+    },
+    %{
+      combo: "ctrl + k",
+      mac_combo: "cmd + k",
+      description: "Focus previous cell"
+    },
+    %{
+      combo: "ctrl + alt + n",
+      mac_combo: "cmd + opt + n",
+      description: "Insert Elixir cell below"
+    },
+    %{
+      combo: "ctrl + alt + m",
+      mac_combo: "cmd + opt + m",
+      description: "Insert Markdown cell below"
+    },
+    %{
+      combo: "ctrl + alt + w",
+      mac_combo: "cmd + opt + w",
+      description: "Delete cell"
+    }
   ]
 
   @impl true
@@ -31,8 +71,11 @@ defmodule LiveBookWeb.ShortcutsComponent do
           <%= for shortcut <- @shortcuts do %>
             <li class="flex items-center my-3">
               <span class="w-1/3">
-                <span class="bg-editor text-editor py-1 px-2 rounded-md">
-                  <%= shortcut.combination %>
+                <span class="non-mac-only bg-editor text-editor py-1 px-2 rounded-md">
+                  <%= shortcut.combo %>
+                </span>
+                <span class="mac-only bg-editor text-editor py-1 px-2 rounded-md">
+                  <%= shortcut.mac_combo %>
                 </span>
               </span>
               <span class="w-2/3">
