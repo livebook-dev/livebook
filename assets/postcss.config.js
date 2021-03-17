b5a21854d25cf0d8119ea1785e6accf6b16c4269
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-03-17 11:17:52 +0100

    Use tailwindcss jit (#79)

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
-};
+  plugins: {
+    'postcss-import': {},
+    '@tailwindcss/jit': {},
+    autoprefixer: {},
+  }
+}
