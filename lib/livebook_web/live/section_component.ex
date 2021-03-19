af8e178b4dff6882eb86b243fe7aed3af4e22448
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-03-19 15:48:17 +0100

    Show section actions on focus or hover only

--- lib/livebook_web/live/section_component.ex
+++ lib/livebook_web/live/section_component.ex
@@ -4,21 +4,19 @@ defmodule LivebookWeb.SectionComponent do
   def render(assigns) do
     ~L"""
     <div data-element="section" data-section-id="<%= @section.id %>">
-      <div class="flex space-x-4 items-center">
-        <div class="flex-grow">
-          <h2 class="flex-grow text-gray-800 font-semibold text-2xl px-1 -ml-1 rounded-lg border  border-transparent hover:border-blue-200 focus:border-blue-300"
-            data-element="section-name"
-            id="section-<%= @section.id %>-name"
-            contenteditable
-            spellcheck="false"
-            phx-blur="set_section_name"
-            phx-value-section_id="<%= @section.id %>"
-            phx-hook="ContentEditable"
-            data-update-attribute="phx-value-name"><%= @section.name %></h2>
-            <%# ^ Note it's important there's no space between <h2> and </h2>
-              because we want the content to exactly match @section.name. %>
-        </div>
-        <div class="flex space-x-2 items-center">
+      <div class="flex space-x-4 items-center" data-element="section-headline">
+        <h2 class="flex-grow text-gray-800 font-semibold text-2xl px-1 -ml-1 rounded-lg border  border-transparent hover:border-blue-200 focus:border-blue-300"
+          data-element="section-name"
+          id="section-<%= @section.id %>-name"
+          contenteditable
+          spellcheck="false"
+          phx-blur="set_section_name"
+          phx-value-section_id="<%= @section.id %>"
+          phx-hook="ContentEditable"
+          data-update-attribute="phx-value-name"><%= @section.name %></h2>
+          <%# ^ Note it's important there's no space between <h2> and </h2>
+            because we want the content to exactly match @section.name. %>
+        <div class="flex space-x-2 items-center" data-element="section-actions">
           <button phx-click="delete_section" phx-value-section_id="<%= @section.id %>" tabindex="-1">
             <%= remix_icon("delete-bin-6-line", class: "text-xl action-icon") %>
           </button>
