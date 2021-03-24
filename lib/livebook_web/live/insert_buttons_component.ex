3f9f3c1909cbbd784ebd0db2360a83db0053dae0
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-03-24 19:21:28 +0100

    Move buttons rendering to the buttons group component

--- lib/livebook_web/live/insert_buttons_component.ex
+++ lib/livebook_web/live/insert_buttons_component.ex
@@ -5,7 +5,24 @@ defmodule LivebookWeb.InsertButtonsComponent do
     ~L"""
     <div class="relative top-0.5 m-0 flex justify-center">
       <div class="absolute z-10 <%= if(@persistent, do: "opacity-100", else: "opacity-0") %> hover:opacity-100 focus-within:opacity-100 flex space-x-2 justify-center items-center">
-        <%= render_block(@inner_block) %>
+        <button class="button button-sm"
+          phx-click="insert_cell"
+          phx-value-type="markdown"
+          phx-value-section_id="<%= @section_id %>"
+          phx-value-index="<%= @insert_cell_index %>"
+          >+ Markdown</button>
+        <button class="button button-sm"
+          phx-click="insert_cell"
+          phx-value-type="elixir"
+          phx-value-section_id="<%= @section_id %>"
+          phx-value-index="<%= @insert_cell_index %>"
+          >+ Elixir</button>
+        <%= if @insert_section_index do %>
+          <button class="button button-sm"
+            phx-click="insert_section"
+            phx-value-index="<%= @insert_section_index %>"
+            >+ Section</button>
+        <% end %>
       </div>
     </div>
     """
