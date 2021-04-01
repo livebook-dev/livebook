fb9a4e3e8bf332f3ff1ff655ed88f33ebb8dd1f0
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-04-02 01:00:13 +0200

    Update lib/livebook_web/views/error_view.ex
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- lib/livebook_web/views/error_view.ex
+++ lib/livebook_web/views/error_view.ex
@@ -1,10 +1,7 @@
 defmodule LivebookWeb.ErrorView do
   use LivebookWeb, :view
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  def template_not_found(_template, assigns) do
+    render("500.html", assigns)
   end
 end
