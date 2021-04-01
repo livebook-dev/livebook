d415c377a9d446679c684d4ba57d6b817f4adaef
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-04-01 15:02:28 +0200

    Highlight results in path selector

--- test/livebook_web/live/path_select_component_test.exs
+++ test/livebook_web/live/path_select_component_test.exs
@@ -16,19 +16,8 @@ defmodule LivebookWeb.PathSelectComponentTest do
     assert render_component(PathSelectComponent, attrs(path: path)) =~ "notebooks"
   end
 
-  test "lists only files with matching name" do
-    path = notebooks_path() |> Path.join("with_two_sectio")
-    assert render_component(PathSelectComponent, attrs(path: path)) =~ "with_two_sections.livemd"
-    refute render_component(PathSelectComponent, attrs(path: path)) =~ "basic.livemd"
-  end
-
   test "does not show parent directory when in root" do
     path = "/"
-    refute render_component(PathSelectComponent, attrs(path: path)) =~ ".."
-  end
-
-  test "does not show parent directory when there is a basename typed" do
-    path = notebooks_path() |> Path.join("a")
     refute render_component(PathSelectComponent, attrs(path: path)) =~ ".."
   end
 
