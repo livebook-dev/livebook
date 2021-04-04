bbfc1989d64b58aa1311bf54e7d26e7086ea1b93
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-04-04 15:10:09 +0200

    Update lib/livebook_web/live/session_live/cell_upload_component.ex
    Co-authored-by: José Valim <jose.valim@dashbit.co>

--- lib/livebook_web/live/session_live/cell_upload_component.ex
+++ lib/livebook_web/live/session_live/cell_upload_component.ex
@@ -17,7 +17,7 @@ defmodule LivebookWeb.SessionLive.CellUploadComponent do
       </h3>
       <%= if @uploads.cell_image.errors != [] do %>
         <div class="mb-3 rounded-lg px-4 py-2 bg-red-100 text-red-400 font-medium">
-          Invalid image file. The image must be either JPEG or PNG and cannot exceed 2MB in size.
+          Invalid image file. The image must be either GIF, JPEG, or PNG and cannot exceed 5MB in size.
         </div>
       <% end %>
       <%= for entry <- @uploads.cell_image.entries do %>
