a977957abc2766a9ae062a4b94b0d719bb5e1e02
Michael Crumm <mike@crumm.net>
2021-04-20 09:48:50 -0700

    Replace NProgress by topbar (#229)

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ import "@fontsource/jetbrains-mono";
 
 import "phoenix_html";
 import { Socket } from "phoenix";
-import NProgress from "nprogress";
+import topbar from "topbar";
 import { LiveSocket } from "phoenix_live_view";
 import ContentEditable from "./content_editable";
 import Cell from "./cell";
@@ -39,8 +39,9 @@ const liveSocket = new LiveSocket("/live", Socket, {
 });
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", (info) => NProgress.start());
-window.addEventListener("phx:page-loading-stop", (info) => NProgress.done());
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", () => topbar.show());
+window.addEventListener("phx:page-loading-stop", () => topbar.hide());
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
