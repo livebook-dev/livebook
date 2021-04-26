790b9e764bcfebce76556a562dc08569fd35161d
Javier Goday <jgoday@gmail.com>
2021-04-26 14:22:33 +0200

    Feature cell link (#239)
    * #99: Add anchor links to sections and cells
    
    * Restores some css classes removed by error
    
    * Focus cell based on anchor link
    
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- lib/livebook_web/live/session_live/cell_component.ex
+++ lib/livebook_web/live/session_live/cell_component.ex
@@ -19,6 +19,7 @@ defmodule LivebookWeb.SessionLive.CellComponent do
     ~L"""
     <div class="mb-1 flex items-center justify-end">
       <div class="relative z-10 flex items-center justify-end space-x-2" data-element="actions">
+        <%= render_cell_anchor_link(@cell_view) %>
         <span class="tooltip top" aria-label="Edit content" data-element="enable-insert-mode-button">
           <button class="icon-button">
             <%= remix_icon("pencil-line", class: "text-xl") %>
@@ -97,6 +98,7 @@ defmodule LivebookWeb.SessionLive.CellComponent do
         <% end %>
       </div>
       <div class="relative z-10 flex items-center justify-end space-x-2" data-element="actions">
+        <%= render_cell_anchor_link(@cell_view) %>
         <span class="tooltip top" aria-label="Cell settings">
           <%= live_patch to: Routes.session_path(@socket, :cell_settings, @session_id, @cell_view.id), class: "icon-button" do %>
             <%= remix_icon("list-settings-line", class: "text-xl") %>
@@ -160,6 +162,18 @@ defmodule LivebookWeb.SessionLive.CellComponent do
         </div>
       <% end %>
     </div>
+    """
+  end
+
+  defp render_cell_anchor_link(cell_view) do
+    assigns = %{cell_view: cell_view}
+
+    ~L"""
+    <span class="tooltip top" aria-label="Link">
+      <a href="#cell-<%= @cell_view.id %>" class="icon-button">
+        <%= remix_icon("link", class: "text-xl") %>
+      </a>
+    </span>
     """
   end
 
