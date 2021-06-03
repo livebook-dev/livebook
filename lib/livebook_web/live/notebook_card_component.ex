91a9f62fb96fda902cfb5cc7bdaacb167e69857f
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-06-03 14:55:25 +0200

    Make images in explore cards clickable

--- lib/livebook_web/live/notebook_card_component.ex
+++ lib/livebook_web/live/notebook_card_component.ex
@@ -5,9 +5,10 @@ defmodule LivebookWeb.NotebookCardComponent do
   def render(assigns) do
     ~L"""
     <div class="flex flex-col">
-      <div class="flex items-center justify-center p-6 border-2 border-gray-100 rounded-t-2xl h-[150px]">
+      <%= live_redirect to: Routes.explore_path(@socket, :notebook, @notebook_info.slug),
+            class: "flex items-center justify-center p-6 border-2 border-gray-100 rounded-t-2xl h-[150px]" do %>
         <img src="<%= @notebook_info.image_url %>" class="max-h-full max-w-[75%]" />
-      </div>
+      <% end %>
       <div class="px-6 py-4 bg-gray-100 rounded-b-2xl flex-grow">
         <%= live_redirect @notebook_info.title,
               to: Routes.explore_path(@socket, :notebook, @notebook_info.slug),
