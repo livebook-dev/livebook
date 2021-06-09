afa2376b73f60b73125c1912b81f47880d729017
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-06-09 15:27:59 +0200

    Clean up tests

---
+++ test/support/noop_runtime.ex
@@ -0,0 +1,19 @@
+defmodule Livebook.Runtime.NoopRuntime do
+  @moduledoc false
+
+  # A runtime that doesn't do any actual evaluation,
+  # thus not requiring any underlying resources.
+
+  defstruct []
+
+  def new(), do: %__MODULE__{}
+
+  defimpl Livebook.Runtime do
+    def connect(_), do: :ok
+    def disconnect(_), do: :ok
+    def evaluate_code(_, _, _, _, _, _ \\ []), do: :ok
+    def forget_evaluation(_, _, _), do: :ok
+    def drop_container(_, _), do: :ok
+    def request_completion_items(_, _, _, _, _, _), do: :ok
+  end
+end
