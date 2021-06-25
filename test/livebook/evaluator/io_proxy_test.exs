579fe821ef07b09db3500c803f4a99539950dc27
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-06-25 14:15:11 +0200

    Implement :get_until in IOProxy (#401)
    * Implement :get_until in IOProxy
    
    * Port get_until tests from StringIO

--- test/livebook/evaluator/io_proxy_test.exs
+++ test/livebook/evaluator/io_proxy_test.exs
@@ -37,12 +37,7 @@ defmodule Livebook.Evaluator.IOProxyTest do
     end
 
     test "IO.gets", %{io: io} do
-      pid =
-        spawn_link(fn ->
-          reply_to_input_request(:ref, "name: ", {:ok, "Jake Peralta"}, 1)
-        end)
-
-      IOProxy.configure(io, pid, :ref)
+      configure_owner_with_input(io, "name: ", "Jake Peralta")
 
       assert IO.gets(io, "name: ") == "Jake Peralta"
     end
@@ -57,15 +52,121 @@ defmodule Livebook.Evaluator.IOProxyTest do
 
       assert IO.gets(io, "name: ") == {:error, "no matching Livebook input found"}
     end
+
+    test "IO.getn with unicode input", %{io: io} do
+      configure_owner_with_input(io, "name: ", "🐈 test\n")
+
+      assert IO.getn(io, "name: ", 3) == "🐈 t"
+    end
+
+    test "IO.getn returns the given number of characters", %{io: io} do
+      configure_owner_with_input(io, "name: ", "Jake Peralta\nAmy Santiago\n")
+
+      assert IO.getn(io, "name: ", 13) == "Jake Peralta\n"
+      assert IO.getn(io, "name: ", 13) == "Amy Santiago\n"
+      assert IO.getn(io, "name: ", 13) == :eof
+    end
+
+    test "IO.getn returns all characters if requested more than available", %{io: io} do
+      configure_owner_with_input(io, "name: ", "Jake Peralta\nAmy Santiago\n")
+
+      assert IO.getn(io, "name: ", 10_000) == "Jake Peralta\nAmy Santiago\n"
+    end
+  end
+
+  # See https://github.com/elixir-lang/elixir/blob/v1.12.1/lib/elixir/test/elixir/string_io_test.exs
+  defmodule GetUntilCallbacks do
+    def until_eof(continuation, :eof) do
+      {:done, continuation, :eof}
+    end
+
+    def until_eof(continuation, content) do
+      {:more, continuation ++ content}
+    end
+
+    def until_eof_then_try_more('magic-stop-prefix' ++ continuation, :eof) do
+      {:done, continuation, :eof}
+    end
+
+    def until_eof_then_try_more(continuation, :eof) do
+      {:more, 'magic-stop-prefix' ++ continuation}
+    end
+
+    def until_eof_then_try_more(continuation, content) do
+      {:more, continuation ++ content}
+    end
+
+    def up_to_3_bytes(continuation, :eof) do
+      {:done, continuation, :eof}
+    end
+
+    def up_to_3_bytes(continuation, content) do
+      case continuation ++ content do
+        [a, b, c | tail] -> {:done, [a, b, c], tail}
+        str -> {:more, str}
+      end
+    end
+
+    def up_to_3_bytes_discard_rest(continuation, :eof) do
+      {:done, continuation, :eof}
+    end
+
+    def up_to_3_bytes_discard_rest(continuation, content) do
+      case continuation ++ content do
+        [a, b, c | _tail] -> {:done, [a, b, c], :eof}
+        str -> {:more, str}
+      end
+    end
+  end
+
+  describe ":get_until" do
+    test "with up_to_3_bytes", %{io: io} do
+      configure_owner_with_input(io, "name: ", "abcdefg")
+
+      result = get_until(io, :unicode, "name: ", GetUntilCallbacks, :up_to_3_bytes)
+      assert result == "abc"
+      assert IO.gets(io, "name: ") == "defg"
+    end
+
+    test "with up_to_3_bytes_discard_rest", %{io: io} do
+      configure_owner_with_input(io, "name: ", "abcdefg")
+
+      result = get_until(io, :unicode, "name: ", GetUntilCallbacks, :up_to_3_bytes_discard_rest)
+      assert result == "abc"
+      assert IO.gets(io, "name: ") == :eof
+    end
+
+    test "with until_eof", %{io: io} do
+      configure_owner_with_input(io, "name: ", "abc\nd")
+
+      result = get_until(io, :unicode, "name: ", GetUntilCallbacks, :until_eof)
+      assert result == "abc\nd"
+    end
+
+    test "with until_eof and \\r\\n", %{io: io} do
+      configure_owner_with_input(io, "name: ", "abc\r\nd")
+
+      result = get_until(io, :unicode, "name: ", GetUntilCallbacks, :until_eof)
+      assert result == "abc\r\nd"
+    end
+
+    test "with until_eof_then_try_more", %{io: io} do
+      configure_owner_with_input(io, "name: ", "abc\nd")
+
+      result = get_until(io, :unicode, "name: ", GetUntilCallbacks, :until_eof_then_try_more)
+      assert result == "abc\nd"
+    end
+
+    test "with raw bytes (latin1)", %{io: io} do
+      configure_owner_with_input(io, "name: ", <<181, 255, 194, ?\n>>)
+
+      result = get_until(io, :latin1, "name: ", GetUntilCallbacks, :until_eof)
+      assert result == <<181, 255, 194, ?\n>>
+    end
   end
 
   test "consumes the given input only once", %{io: io} do
-    pid =
-      spawn_link(fn ->
-        reply_to_input_request(:ref, "name: ", {:ok, "Jake Peralta\nAmy Santiago\n"}, 1)
-      end)
-
-    IOProxy.configure(io, pid, :ref)
+    configure_owner_with_input(io, "name: ", "Jake Peralta\nAmy Santiago\n")
 
     assert IO.gets(io, "name: ") == "Jake Peralta\n"
     assert IO.gets(io, "name: ") == "Amy Santiago\n"
@@ -121,42 +222,20 @@ defmodule Livebook.Evaluator.IOProxyTest do
     assert IOProxy.flush_widgets(io) == MapSet.new()
   end
 
-  test "getn/1 return first character", %{io: io} do
-    pid =
-      spawn_link(fn ->
-        reply_to_input_request(:ref, "name: ", {:ok, "🐈 test\n"}, 1)
-      end)
-
-    IOProxy.configure(io, pid, :ref)
-
-    assert IO.getn(io, "name: ") == "🐈"
-  end
-
-  test "getn/2 returns the number of defined characters ", %{io: io} do
-    pid =
-      spawn_link(fn ->
-        reply_to_input_request(:ref, "name: ", {:ok, "Jake Peralta\nAmy Santiago\n"}, 1)
-      end)
-
-    IOProxy.configure(io, pid, :ref)
-
-    assert IO.getn(io, "name: ", 13) == "Jake Peralta\n"
-    assert IO.getn(io, "name: ", 13) == "Amy Santiago\n"
-    assert IO.getn(io, "name: ", 13) == :eof
-  end
-
-  test "getn/2 all characters", %{io: io} do
-    pid =
-      spawn_link(fn ->
-        reply_to_input_request(:ref, "name: ", {:ok, "Jake Peralta\nAmy Santiago\n"}, 1)
-      end)
-
-    IOProxy.configure(io, pid, :ref)
-
-    assert IO.getn(io, "name: ", 10_000) == "Jake Peralta\nAmy Santiago\n"
-  end
-
   # Helpers
+
+  defp get_until(pid, encoding, prompt, module, function) do
+    :io.request(pid, {:get_until, encoding, prompt, module, function, []})
+  end
+
+  defp configure_owner_with_input(io, prompt, input) do
+    pid =
+      spawn_link(fn ->
+        reply_to_input_request(:ref, prompt, {:ok, input}, 1)
+      end)
+
+    IOProxy.configure(io, pid, :ref)
+  end
 
   defp reply_to_input_request(_ref, _prompt, _reply, 0), do: :ok
 
