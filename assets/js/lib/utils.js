8e5b387f04af354b65ac31c69b20177e580862e8
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-06-26 18:05:52 +0200

    Update VegaLite layout when the data changes (#405)

--- assets/js/lib/utils.js
+++ assets/js/lib/utils.js
@@ -84,3 +84,22 @@ export function randomId() {
 export function md5Base64(string) {
   return md5(string).toString(encBase64);
 }
+
+/**
+ * A simple throttle version that ensures
+ * the given function is called at most once
+ * within the given time window.
+ */
+export function throttle(fn, windowMs) {
+  let ignore = false;
+
+  return (...args) => {
+    if (!ignore) {
+      fn(...args);
+      ignore = true;
+      setTimeout(() => {
+        ignore = false;
+      }, windowMs);
+    }
+  };
+}
