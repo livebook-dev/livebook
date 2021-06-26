0702a21ca83244c6211732d5b0f238f004542958
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-06-26 14:54:25 +0200

    Make cell indicator absolute

--- lib/livebook_web/live/session_live/cell_component.ex
+++ lib/livebook_web/live/session_live/cell_component.ex
@@ -57,8 +57,8 @@ defmodule LivebookWeb.SessionLive.CellComponent do
       </div>
     </div>
 
-    <div class="flex">
-      <div class="w-1 rounded-lg relative -left-3" data-element="cell-focus-indicator">
+    <div class="flex relative">
+      <div class="w-1 h-full rounded-lg absolute top-0 -left-3" data-element="cell-focus-indicator">
       </div>
       <div class="w-full">
         <div class="pb-4" data-element="editor-box">
@@ -130,8 +130,8 @@ defmodule LivebookWeb.SessionLive.CellComponent do
       </div>
     </div>
 
-    <div class="flex">
-      <div class="w-1 rounded-lg relative -left-3" data-element="cell-focus-indicator">
+    <div class="flex relative">
+      <div class="w-1 h-full rounded-lg absolute top-0 -left-3" data-element="cell-focus-indicator">
       </div>
       <div class="w-full">
         <%= render_editor(assigns) %>
@@ -182,8 +182,8 @@ defmodule LivebookWeb.SessionLive.CellComponent do
       </div>
     </div>
 
-    <div class="flex">
-      <div class="w-1 rounded-lg relative -left-3" data-element="cell-focus-indicator">
+    <div class="flex relative">
+      <div class="w-1 h-full rounded-lg absolute top-0 -left-3" data-element="cell-focus-indicator">
       </div>
       <div>
         <form phx-change="set_cell_value" phx-submit="queue_bound_cells_evaluation">
