123b9226e44d6a92f33291dd4a3ca882c6a6c9df
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-08-12 14:16:08 +0200

    Update earmark_parser

--- mix.exs
+++ mix.exs
@@ -51,9 +51,7 @@ defmodule Livebook.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      # {:earmark_parser, "~> 1.4"},
-      {:earmark_parser, "~> 1.4",
-       github: "jonatanklosko/earmark_parser", branch: "jk-optional-inline"},
+      {:earmark_parser, "~> 1.4"},
       {:bypass, "~> 2.1", only: :test},
       {:castore, "~> 0.1.0"}
     ]
