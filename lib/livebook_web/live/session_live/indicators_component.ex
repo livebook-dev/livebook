adccdcefce440bddb644e01135c64af6629eae49
Owen Bickford <owen+github@owencode.com>
2021-10-24 11:48:18 -0400

    add back-to-top link, class autosorting/formatting

--- lib/livebook_web/live/session_live/indicators_component.ex
+++ lib/livebook_web/live/session_live/indicators_component.ex
@@ -4,7 +4,7 @@ defmodule LivebookWeb.SessionLive.IndicatorsComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col space-y-2 items-center" data-element="notebook-indicators">
+    <div class="flex flex-col items-center space-y-2" data-element="notebook-indicators">
       <%= if @file do %>
         <%= if @dirty do %>
           <%= if @autosave_interval_s do %>
@@ -52,9 +52,15 @@ defmodule LivebookWeb.SessionLive.IndicatorsComponent do
         </span>
       <% end %>
 
+      <span class="tooltip left" aria-label="Go back to the top of this page">
+        <a href="#notebook-name" class="border-gray-200 icon-button icon-outlined-button hover:bg-gray-100 focus:bg-gray-100">
+          <.remix_icon icon="arrow-up-line" class="text-xl text-gray-400" />
+        </a>
+      </span>
+
       <%# Note: this indicator is shown/hidden using CSS based on the current mode %>
       <span class="tooltip left" aria-label="Insert mode" data-element="insert-mode-indicator">
-        <span class="text-sm text-gray-400 font-medium cursor-default">
+        <span class="text-sm font-medium text-gray-400 cursor-default">
           ins
         </span>
       </span>
@@ -65,7 +71,7 @@ defmodule LivebookWeb.SessionLive.IndicatorsComponent do
   defp global_evaluation_status(%{status: :evaluating} = assigns) do
     ~H"""
     <span class="tooltip left" aria-label="Go to evaluating cell">
-      <button class="icon-button icon-outlined-button border-blue-400 hover:bg-blue-50 focus:bg-blue-50"
+      <button class="border-blue-400 icon-button icon-outlined-button hover:bg-blue-50 focus:bg-blue-50"
         data-element="focus-cell-button"
         data-target={@cell_id}>
         <.remix_icon icon="loader-3-line" class="text-xl text-blue-500 animate-spin" />
@@ -77,7 +83,7 @@ defmodule LivebookWeb.SessionLive.IndicatorsComponent do
   defp global_evaluation_status(%{status: :evaluated} = assigns) do
     ~H"""
     <span class="tooltip left" aria-label="Go to last evaluated cell">
-      <button class="icon-button icon-outlined-button border-green-300 hover:bg-green-50 focus:bg-green-50"
+      <button class="border-green-300 icon-button icon-outlined-button hover:bg-green-50 focus:bg-green-50"
         data-element="focus-cell-button"
         data-target={@cell_id}>
         <.remix_icon icon="loader-3-line" class="text-xl text-green-400" />
@@ -89,7 +95,7 @@ defmodule LivebookWeb.SessionLive.IndicatorsComponent do
   defp global_evaluation_status(%{status: :stale} = assigns) do
     ~H"""
     <span class="tooltip left" aria-label="Go to first stale cell">
-      <button class="icon-button icon-outlined-button border-yellow-200 hover:bg-yellow-50 focus:bg-yellow-50"
+      <button class="border-yellow-200 icon-button icon-outlined-button hover:bg-yellow-50 focus:bg-yellow-50"
         data-element="focus-cell-button"
         data-target={@cell_id}>
         <.remix_icon icon="loader-3-line" class="text-xl text-yellow-300" />
@@ -101,7 +107,7 @@ defmodule LivebookWeb.SessionLive.IndicatorsComponent do
   defp global_evaluation_status(%{status: :fresh} = assigns) do
     ~H"""
     <span class="tooltip left" aria-label="Ready to evaluate">
-      <button class="icon-button icon-outlined-button border-gray-200 hover:bg-gray-100 focus:bg-gray-100 cursor-default">
+      <button class="border-gray-200 cursor-default icon-button icon-outlined-button hover:bg-gray-100 focus:bg-gray-100">
         <.remix_icon icon="loader-3-line" class="text-xl text-gray-400" />
       </button>
     </span>
