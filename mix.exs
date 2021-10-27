5d24510d506e74e3b3c92fd97fb66211c75b002a
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-10-27 17:05:17 +0200

    Release 0.3.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Livebook.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @description "Interactive and collaborative code notebooks - made with Phoenix LiveView"
 
   def project do
