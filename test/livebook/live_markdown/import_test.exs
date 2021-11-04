057dad75926a4b25209a60ac8f088c413167b6e6
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-11-04 11:04:17 +0000

    keep the test focused

--- test/livebook/live_markdown/import_test.exs
+++ test/livebook/live_markdown/import_test.exs
@@ -660,7 +660,7 @@ defmodule Livebook.LiveMarkdown.ImportTest do
     ```
 
     ```vega-lite
-    "$schema":"https://vega.github.io/schema/vega-lite/v5.json","data":{"values":[{"in":1,"out":1},{"in":2,"out":2},{"in":3,"out":3},{"in":4,"out":4},{"in":5,"out":5}]},"encoding":{"x":{"field":"in","type":"quantitative"},"y":{"field":"out","type":"quantitative"}},"height":200,"mark":"line","width":500}
+    not_a_json
     ```
     """
 
