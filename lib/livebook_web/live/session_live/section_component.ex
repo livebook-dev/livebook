5fcad5d81a03bda74ded0aec97123772100be8f3
Lee Jarvis <leejarvis@fastmail.com>
2021-12-27 17:23:08 +0000

    Fix indentation

--- lib/livebook_web/live/session_live/section_component.ex
+++ lib/livebook_web/live/session_live/section_component.ex
@@ -77,12 +77,12 @@ defmodule LivebookWeb.SessionLive.SectionComponent do
             {if @section_view.has_children?,
                do: [class: "tooltip left", data_tooltip: "Cannot delete this section because\nother sections branch from it"],
                else: [class: "tooltip top", data_tooltip: "Delete"]}>
-              <button class={"icon-button #{if @section_view.has_children?, do: "disabled"}"}
-                aria-label="delete section"
-                phx-click="delete_section"
-                phx-value-section_id={@section_view.id}>
-                <.remix_icon icon="delete-bin-6-line" class="text-xl" />
-              </button>
+            <button class={"icon-button #{if @section_view.has_children?, do: "disabled"}"}
+              aria-label="delete section"
+              phx-click="delete_section"
+              phx-value-section_id={@section_view.id}>
+              <.remix_icon icon="delete-bin-6-line" class="text-xl" />
+            </button>
           </span>
         </div>
       </div>
