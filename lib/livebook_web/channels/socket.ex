cd6c23b736aef27cd1f5825aade43aa5e53587bc
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-01-06 13:33:57 +0100

    Authenticate socket connection from session

--- lib/livebook_web/channels/socket.ex
+++ lib/livebook_web/channels/socket.ex
@@ -10,7 +10,13 @@ defmodule LivebookWeb.Socket do
 
   @impl true
   def connect(params, socket, info) do
-    Phoenix.LiveView.Socket.connect(params, socket, info)
+    auth_mode = Livebook.Config.auth_mode()
+
+    if LivebookWeb.AuthPlug.authenticated?(info.session || %{}, info.uri.port, auth_mode) do
+      Phoenix.LiveView.Socket.connect(params, socket, info)
+    else
+      :error
+    end
   end
 
   @impl true
