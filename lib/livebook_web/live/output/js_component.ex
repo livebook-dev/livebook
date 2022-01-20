da86678de24e02a3ac7e5e2ee24ee0c949867374
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-01-20 11:18:48 +0100

    Compute iframe base URL based on page origin

--- lib/livebook_web/live/output/js_component.ex
+++ lib/livebook_web/live/output/js_component.ex
@@ -8,7 +8,7 @@ defmodule LivebookWeb.Output.JSComponent do
       phx-hook="JSOutput"
       phx-update="ignore"
       data-ref={@info.ref}
-      data-assets-base-url={Routes.session_url(@socket, :show_asset, @session_id, @info.assets.hash, [])}
+      data-assets-base-path={Routes.session_path(@socket, :show_asset, @session_id, @info.assets.hash, [])}
       data-js-path={@info.assets.js_path}
       data-session-token={session_token(@info.pid)}
       data-session-id={@session_id}>
