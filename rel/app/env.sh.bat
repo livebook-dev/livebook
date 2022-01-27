37959cde1d7c33da6512586b2ee570cbe23a91b4
José Valim <jose.valim@dashbit.co>
2022-01-27 13:39:51 +0100

    Copy and paste

--- rel/app/env.sh.bat
+++ rel/app/env.sh.bat
@@ -1,2 +1,6 @@
-set RELEASE_MODE=interactive
 set LIVEBOOK_ROOT_PATH=%USERPROFILE%
+set RELEASE_MODE=interactive
+if not defined RELEASE_COOKIE (
+  for /f "skip=1" %%X in ('wmic os get localdatetime') do if not defined TIMESTAMP set TIMESTAMP=%%X
+  set RELEASE_COOKIE=cookie-!TIMESTAMP:~0,11!-!RANDOM!
+)
