d11090b4f91ee1f2cbfc1236f2c7e4679ad9c2ac
Wojtek Mach <wojtekmach@users.noreply.github.com>
2022-02-15 12:44:59 +0100

    Create release cookie in env scripts (#1004)

--- rel/app/env.sh.bat
+++
@@ -1,5 +0,0 @@
-set RELEASE_MODE=interactive
-if not defined RELEASE_COOKIE (
-  for /f "skip=1" %%X in ('wmic os get localdatetime') do if not defined TIMESTAMP set TIMESTAMP=%%X
-  set RELEASE_COOKIE=cookie-!TIMESTAMP:~0,11!-!RANDOM!
-)
