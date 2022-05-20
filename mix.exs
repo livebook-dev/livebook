3e431e99422c6875075f10d5c43888214abe4871
Wojtek Mach <wojtek@wojtekmach.pl>
2022-05-20 14:18:44 +0200

    Run Mac app in the background
    We don't have regular GUI, just the menu bar icon. So with this setting,
    we won't show up in the Dock.

--- mix.exs
+++ mix.exs
@@ -192,12 +192,18 @@ defmodule Livebook.MixProject do
   ]
 
   defp build_mac_app(release) do
-    AppBuilder.build_mac_app(release, @app_options)
+    options =
+      [
+        uielement: true
+      ] ++ @app_options
+
+    AppBuilder.build_mac_app(release, options)
   end
 
   defp build_mac_app_dmg(release) do
     options =
       [
+        uielement: true,
         codesign: [
           identity: System.fetch_env!("CODESIGN_IDENTITY")
         ],
