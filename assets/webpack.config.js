2e29167229ad4d6f517790482787e86f5b006836
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-06-24 11:00:11 +0100

    Highlight HTML in Markdown snippets (#1244)

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = (env, options) => {
       }),
       new MiniCssExtractPlugin({ filename: "../css/app.css" }),
       new MonacoWebpackPlugin({
-        languages: ["markdown", "elixir", "xml", "json", "sql"],
+        languages: ["markdown", "elixir", "xml", "json", "sql", "html"],
       }),
     ],
     optimization: {
