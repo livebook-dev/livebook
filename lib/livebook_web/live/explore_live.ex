8f2e48433d5ed62c5af948e74bf07cc736fee174
José Valim <jose.valim@dashbit.co>
2022-08-03 11:36:49 +0200

    Updates

--- lib/livebook_web/live/explore_live.ex
+++ lib/livebook_web/live/explore_live.ex
@@ -25,7 +25,11 @@ defmodule LivebookWeb.ExploreLive do
   def render(assigns) do
     ~H"""
     <div class="flex grow h-full">
-      <SidebarHelpers.new_sidebar socket={@socket} current={Routes.explore_path(@socket, :page)} />
+      <SidebarHelpers.new_sidebar
+        socket={@socket}
+        current_page={Routes.explore_path(@socket, :page)}
+        current_user={@current_user}
+      />
       <div class="grow px-6 py-8 overflow-y-auto">
         <div class="max-w-screen-md w-full mx-auto px-4 pb-8 space-y-8">
           <div>
