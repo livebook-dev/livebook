603ffe9f0758c983affd049b554456099283e9dc
Alexandre de Souza <alexandre@aledsz.com.br>
2022-08-08 16:36:06 -0300

    Apply npm run format

--- assets/js/hooks/select_hub_service.js
+++ assets/js/hooks/select_hub_service.js
@@ -10,10 +10,10 @@ import { getAttributeOrThrow } from "../lib/attribute";
 const SelectHubService = {
   mounted() {
     this.el.addEventListener("click", (_) => {
-      const service = getAttributeOrThrow(this.el, "id")
+      const service = getAttributeOrThrow(this.el, "id");
       this.pushEvent("select_hub_service", { value: service });
     });
-  }
-}
+  },
+};
 
 export default SelectHubService;
