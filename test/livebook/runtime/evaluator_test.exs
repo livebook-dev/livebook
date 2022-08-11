9bd1134700aea24928a2ce2dd643ec7b04210817
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-08-11 18:30:19 +0200

    Fix tests on OTP25

--- test/livebook/runtime/evaluator_test.exs
+++ test/livebook/runtime/evaluator_test.exs
@@ -97,8 +97,11 @@ defmodule Livebook.Runtime.EvaluatorTest do
       Evaluator.evaluate_code(evaluator, code, :code_1, nil, file: "file.ex")
 
       assert_receive {:runtime_evaluation_response, :code_1,
-                      {:error, :error, :function_clause, [{List, :first, _arity, _location}]},
-                      metadata()}
+                      {:error, :error, :function_clause,
+                       [
+                         {List, :first, _arity, _location1},
+                         {:elixir_eval, :__FILE__, 1, _location2}
+                       ]}, metadata()}
     end
 
     test "returns additional metadata when there is a syntax error", %{evaluator: evaluator} do
@@ -138,7 +141,9 @@ defmodule Livebook.Runtime.EvaluatorTest do
 
       Evaluator.evaluate_code(evaluator, code, :code_1, nil, file: "file.ex")
 
-      expected_stacktrace = [{Code, :validated_eval_string, 3, [file: 'lib/code.ex', line: 404]}]
+      expected_stacktrace = [
+        {:elixir_eval, :__FILE__, 1, [file: 'file.ex', line: 1]}
+      ]
 
       assert_receive {:runtime_evaluation_response, :code_1,
                       {:error, :error, %CompileError{}, ^expected_stacktrace}, %{code_error: nil}}
@@ -169,7 +174,8 @@ defmodule Livebook.Runtime.EvaluatorTest do
 
         expected_stacktrace = [
           {Livebook.EvaluatorTest.Stacktrace.Math, :bad_math, 0, [file: 'nofile', line: 3]},
-          {Livebook.EvaluatorTest.Stacktrace.Cat, :meow, 0, [file: 'nofile', line: 10]}
+          {Livebook.EvaluatorTest.Stacktrace.Cat, :meow, 0, [file: 'nofile', line: 10]},
+          {:elixir_eval, :__FILE__, 1, [file: 'nofile', line: 15]}
         ]
 
         # Note: evaluating module definitions is relatively slow, so we use a higher wait timeout.
