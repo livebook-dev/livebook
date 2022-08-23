bb243a596480d2082ead12999b57b0147d510b6f
José Valim <jose.valim@dashbit.co>
2022-08-23 08:43:45 +0200

    Remove comment field from policy

--- lib/livebook_web/live/hooks/policy_hook.ex
+++ lib/livebook_web/live/hooks/policy_hook.ex
@@ -2,6 +2,6 @@ defmodule LivebookWeb.PolicyHook do
   import Phoenix.LiveView
 
   def on_mount(:private, _params, _session, socket) do
-    {:cont, socket |> assign(:policy, %{read: true, execute: true, comment: true, edit: true})}
+    {:cont, socket |> assign(:policy, %{read: true, execute: true, edit: true})}
   end
 end
