c875f57d7d7eed3738475f9e73853b5c95edfc35
Alexandre de Souza <alexandre@aledsz.com.br>
2022-08-31 14:37:18 -0300

    Allow users to handle secrets from Fly application

--- lib/livebook_web/live/hub/edit/fly_component.ex
+++ lib/livebook_web/live/hub/edit/fly_component.ex
@@ -14,13 +14,20 @@ defmodule LivebookWeb.Hub.Edit.FlyComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(app_url: "https://#{app["hostname"]}", changeset: changeset)}
+     |> assign(
+       app_url: "https://#{app["hostname"]}",
+       changeset: changeset,
+       secrets: app["secrets"],
+       secret_data: %{},
+       operation: :new,
+       valid_secret?: false
+     )}
   end
 
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
+    <div id={@id <> "-component"}>
       <!-- System details -->
       <div class="flex flex-col space-y-2 pb-5">
         <div class="flex items-center justify-between border border-gray-200 rounded-lg p-4">
@@ -103,13 +110,190 @@ defmodule LivebookWeb.Hub.Edit.FlyComponent do
           ) %>
         </.form>
       </div>
+
+      <div class="flex flex-col mt-4 space-y-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 w-full border-b pb-2 border-gray-200">
+          <div class="place-content-start">
+            <h2 class="text-xl text-gray-800 font-semibold">
+              Secrets
+            </h2>
+          </div>
+
+          <div class="flex place-content-end">
+            <button
+              class="button-base border-1 border-gray-200 hover:bg-gray-200"
+              type="button"
+              phx-click={show_modal("new-secret-modal")}
+            >
+              Add secret
+            </button>
+          </div>
+        </div>
+
+        <div class="mt-4">
+          <div class="border border-gray-200 rounded-lg p-4 mt-0">
+            <%= for {secret, index} <- Enum.with_index(@secrets) do %>
+              <.secret_card
+                myself={@myself}
+                secret={secret}
+                index={index}
+                last_index={length(@secrets) - 1}
+              />
+            <% end %>
+          </div>
+        </div>
+      </div>
+
+      <.secret_modal
+        on_save={hide_modal("secret-modal")}
+        data={@secret_data}
+        valid?={@valid_secret?}
+        myself={@myself}
+      />
     </div>
+    """
+  end
+
+  defp secret_card(assigns) do
+    card_class = "flex items-center justify-between"
+
+    assigns =
+      if assigns.index < assigns.last_index do
+        assign_new(assigns, :class, fn ->
+          card_class <> " mb-3 pb-3 border-b border-gray-200"
+        end)
+      else
+        assign_new(assigns, :class, fn -> card_class end)
+      end
+
+    ~H"""
+    <div id={"secret-" <> @secret["id"]} class={@class}>
+      <div class="grid grid-cols-1 md:grid-cols-3 w-full">
+        <div class="place-content-start">
+          <.labeled_text label="Name">
+            <%= @secret["name"] %>
+          </.labeled_text>
+        </div>
+
+        <div class="flex place-content-end">
+          <.labeled_text label="Created at">
+            <%= @secret["createdAt"] %>
+          </.labeled_text>
+        </div>
+
+        <div class="flex place-content-end">
+          <button
+            id={"secret-" <> @secret["id"] <> "-edit"}
+            phx-click={show_modal("secret-modal") |> JS.push("edit", value: %{secret: @secret})}
+            phx-target={@myself}
+            class="icon-button text-blue-500 hover:text-blue-800"
+          >
+            <.remix_icon icon="file-edit-line" class="align-middle mr-1" />
+            <span>Edit</span>
+          </button>
+          <button
+            id={"secret-" <> @secret["id"] <> "-delete"}
+            phx-click={
+              with_confirm(
+                JS.push("delete", value: %{secret: @secret}),
+                title: "Delete #{@secret["name"]}",
+                description: "Are you sure you want to delete secret?",
+                confirm_text: "Delete",
+                confirm_icon: "delete-bin-6-line"
+              )
+            }
+            phx-target={@myself}
+            class="icon-button text-red-500 hover:text-red-800"
+          >
+            <.remix_icon icon="delete-bin-line" class="align-middle mr-1" />
+            <span>Delete</span>
+          </button>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  defp secret_modal(assigns) do
+    ~H"""
+    <.modal id="secret-modal" class="w-full max-w-sm">
+      <div class="p-6 pb-4 max-w-4xl flex flex-col space-y-5">
+        <h3 class="text-2xl font-semibold text-gray-800">
+          Add secret
+        </h3>
+        <div class="flex-col space-y-5">
+          <p class="text-gray-700">
+            Enter the secret name and its value.
+          </p>
+          <.form
+            id="secret-form"
+            let={f}
+            for={:secret}
+            phx-submit={@on_save |> JS.push("save")}
+            phx-change="validate"
+            autocomplete="off"
+            phx-target={@myself}
+          >
+            <div class="flex flex-col space-y-4">
+              <div>
+                <div class="input-label">
+                  Key <span class="text-xs text-gray-500">(alphanumeric and underscore)</span>
+                </div>
+                <%= text_input(f, :key,
+                  value: @data["key"],
+                  class: "input",
+                  placeholder: "secret key",
+                  autofocus: true,
+                  aria_labelledby: "secret-key",
+                  spellcheck: "false"
+                ) %>
+              </div>
+              <div>
+                <div class="input-label">Value</div>
+                <%= text_input(f, :value,
+                  value: @data["value"],
+                  class: "input",
+                  placeholder: "secret value",
+                  aria_labelledby: "secret-value",
+                  spellcheck: "false"
+                ) %>
+              </div>
+            </div>
+            <%= submit("Add Secret",
+              class: "mt-5 button-base button-blue",
+              phx_disable_with: "Adding...",
+              disabled: not @valid?
+            ) %>
+          </.form>
+        </div>
+      </div>
+    </.modal>
     """
   end
 
   @impl true
   def handle_event("randomize_color", _, socket) do
     handle_event("validate", %{"fly" => %{"hub_color" => HexColor.random()}}, socket)
+  end
+
+  def handle_event("edit", %{"secret" => %{"name" => name}}, socket) do
+    {:noreply, assign(socket, operation: :edit, secret_data: %{"key" => name})}
+  end
+
+  def handle_event("delete", %{"secret" => %{"name" => key}}, socket) do
+    case FlyClient.delete_secrets(socket.assigns.hub, [key]) do
+      {:ok, _} ->
+        {:noreply,
+         socket
+         |> put_flash(:success, "Secret deleted successfully")
+         |> push_redirect(to: Routes.hub_path(socket, :edit, socket.assigns.hub.id))}
+
+      {:error, _} ->
+        {:noreply,
+         socket
+         |> put_flash(:error, "Failed to delete secret")
+         |> push_redirect(to: Routes.hub_path(socket, :edit, socket.assigns.hub.id))}
+    end
   end
 
   def handle_event("save", %{"fly" => params}, socket) do
@@ -125,9 +309,48 @@ defmodule LivebookWeb.Hub.Edit.FlyComponent do
     end
   end
 
+  def handle_event("save", %{"secret" => params}, socket) do
+    if socket.assigns.valid_secret? do
+      case FlyClient.put_secrets(socket.assigns.hub, [params]) do
+        {:ok, _} ->
+          message =
+            if socket.assigns.operation == :new do
+              "Secret added successfully"
+            else
+              "Secret updated successfully"
+            end
+
+          {:noreply,
+           socket
+           |> put_flash(:success, message)
+           |> push_redirect(to: Routes.hub_path(socket, :edit, socket.assigns.hub.id))}
+
+        {:error, _} ->
+          message =
+            if socket.assigns.operation == :new do
+              "Failed to add secret"
+            else
+              "Failed to update secret"
+            end
+
+          {:noreply,
+           socket
+           |> put_flash(:error, message)
+           |> push_redirect(to: Routes.hub_path(socket, :edit, socket.assigns.hub.id))}
+      end
+    else
+      {:noreply, socket}
+    end
+  end
+
   def handle_event("validate", %{"fly" => attrs}, socket) do
     changeset = Fly.change_hub(socket.assigns.hub, attrs)
     {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("validate", %{"secret" => attrs}, socket) do
+    valid? = String.match?(attrs["key"], ~r/^\w+$/) and attrs["value"] not in ["", nil]
+    {:noreply, assign(socket, valid_secret?: valid?, secret_data: attrs)}
   end
 
   defp hub_color(changeset), do: get_field(changeset, :hub_color)
--- test/livebook_web/live/hub/edit_live_test.exs
+++ test/livebook_web/live/hub/edit_live_test.exs
@@ -6,19 +6,33 @@ defmodule LivebookWeb.Hub.EditLiveTest do
   alias Livebook.Hubs
 
   setup do
-    on_exit(&Hubs.clean_hubs/0)
-    :ok
+    on_exit(fn ->
+      Hubs.clean_hubs()
+    end)
+
+    bypass = Bypass.open()
+    Application.put_env(:livebook, :fly_graphql_endpoint, "http://localhost:#{bypass.port}")
+
+    {:ok, bypass: bypass}
   end
 
   describe "fly" do
-    test "updates fly", %{conn: conn} do
-      hub = insert_hub(:fly, id: "fly-987654321", application_id: "987654321")
-      fly_bypass(hub.application_id)
+    test "updates fly", %{conn: conn, bypass: bypass} do
+      {:ok, pid} = Agent.start(fn -> %{fun: &fetch_app_response/2, type: :mount} end)
+
+      app_id = Livebook.Utils.random_short_id()
+      hub = insert_hub(:fly, id: "fly-#{app_id}", application_id: app_id)
+      fly_bypass(bypass, app_id, pid)
 
       {:ok, view, html} = live(conn, Routes.hub_path(conn, :edit, hub.id))
 
       assert html =~ "See app on Fly"
       assert html =~ "https://#{hub.application_id}.fly.dev"
+
+      assert html =~ "Secrets"
+      refute html =~ "FOO_ENV_VAR"
+      assert html =~ "LIVEBOOK_PASSWORD"
+      assert html =~ "LIVEBOOK_SECRET_KEY_BASE"
 
       attrs = %{
         "hub_name" => "Personal Hub",
@@ -47,7 +61,7 @@ defmodule LivebookWeb.Hub.EditLiveTest do
 
       assert view
              |> element("#hubs")
-             |> render() =~ "/hub/fly-987654321"
+             |> render() =~ Routes.hub_path(conn, :edit, hub.id)
 
       assert view
              |> element("#hubs")
@@ -55,20 +69,163 @@ defmodule LivebookWeb.Hub.EditLiveTest do
 
       refute Hubs.fetch_hub!(hub.id) == hub
     end
+
+    test "add secret", %{conn: conn, bypass: bypass} do
+      {:ok, pid} = Agent.start(fn -> %{fun: &fetch_app_response/2, type: :mount} end)
+
+      app_id = Livebook.Utils.random_short_id()
+      hub = insert_hub(:fly, id: "fly-#{app_id}", application_id: app_id)
+      fly_bypass(bypass, app_id, pid)
+
+      {:ok, view, html} = live(conn, Routes.hub_path(conn, :edit, hub.id))
+
+      assert html =~ "See app on Fly"
+      assert html =~ "https://#{hub.application_id}.fly.dev"
+
+      assert html =~ "Secrets"
+      refute html =~ "FOO_ENV_VAR"
+      assert html =~ "LIVEBOOK_PASSWORD"
+      assert html =~ "LIVEBOOK_SECRET_KEY_BASE"
+
+      view
+      |> element("#secret-form")
+      |> render_change(%{"secret" => %{"key" => "FOO_ENV_VAR", "value" => "12345"}})
+
+      refute view
+             |> element("#secret-form button[disabled]")
+             |> has_element?()
+
+      :ok = Agent.update(pid, fn state -> %{state | type: :add} end)
+
+      assert {:ok, _view, html} =
+               view
+               |> element("#secret-form")
+               |> render_submit(%{"secret" => %{"key" => "FOO_ENV_VAR", "value" => "12345"}})
+               |> follow_redirect(conn)
+
+      assert html =~ "Secret added successfully"
+      assert html =~ "Secrets"
+      assert html =~ "FOO_ENV_VAR"
+      assert html =~ "LIVEBOOK_PASSWORD"
+      assert html =~ "LIVEBOOK_SECRET_KEY_BASE"
+    end
+
+    test "update secret", %{conn: conn, bypass: bypass} do
+      {:ok, pid} = Agent.start(fn -> %{fun: &fetch_app_response/2, type: :foo} end)
+
+      old_secret =
+        :foo
+        |> secrets()
+        |> Enum.find(&(&1["name"] == "FOO_ENV_VAR"))
+
+      new_secret =
+        :updated_foo
+        |> secrets()
+        |> Enum.find(&(&1["name"] == "FOO_ENV_VAR"))
+
+      app_id = Livebook.Utils.random_short_id()
+      hub = insert_hub(:fly, id: "fly-#{app_id}", application_id: app_id)
+      fly_bypass(bypass, app_id, pid)
+
+      {:ok, view, html} = live(conn, Routes.hub_path(conn, :edit, hub.id))
+
+      assert html =~ "See app on Fly"
+      assert html =~ "https://#{hub.application_id}.fly.dev"
+
+      assert html =~ "Secrets"
+      assert html =~ "FOO_ENV_VAR"
+      assert html =~ old_secret["createdAt"]
+
+      view
+      |> element("#secret-#{old_secret["id"]}-edit")
+      |> render_click(%{"secret" => old_secret})
+
+      view
+      |> element("#secret-form")
+      |> render_change(%{"secret" => %{"key" => "FOO_ENV_VAR", "value" => "12345"}})
+
+      refute view
+             |> element("#secret-form button[disabled]")
+             |> has_element?()
+
+      :ok = Agent.update(pid, fn state -> %{state | type: :updated_foo} end)
+
+      assert {:ok, _view, html} =
+               view
+               |> element("#secret-form")
+               |> render_submit(%{"secret" => %{"key" => "FOO_ENV_VAR", "value" => "12345"}})
+               |> follow_redirect(conn)
+
+      assert html =~ "Secret updated successfully"
+      assert html =~ "Secrets"
+      assert html =~ "FOO_ENV_VAR"
+      assert html =~ new_secret["createdAt"]
+    end
+
+    test "delete secret", %{conn: conn, bypass: bypass} do
+      {:ok, pid} = Agent.start(fn -> %{fun: &fetch_app_response/2, type: :add} end)
+
+      secret =
+        :add
+        |> secrets()
+        |> Enum.find(&(&1["name"] == "FOO_ENV_VAR"))
+
+      app_id = Livebook.Utils.random_short_id()
+      hub = insert_hub(:fly, id: "fly-#{app_id}", application_id: app_id)
+      fly_bypass(bypass, app_id, pid)
+
+      {:ok, view, html} = live(conn, Routes.hub_path(conn, :edit, hub.id))
+
+      assert html =~ "See app on Fly"
+      assert html =~ "https://#{hub.application_id}.fly.dev"
+
+      assert html =~ "Secrets"
+      assert html =~ "FOO_ENV_VAR"
+      assert html =~ "LIVEBOOK_PASSWORD"
+      assert html =~ "LIVEBOOK_SECRET_KEY_BASE"
+
+      :ok = Agent.update(pid, fn state -> %{state | type: :mount} end)
+
+      assert {:ok, _view, html} =
+               view
+               |> with_target("#fly-form-component")
+               |> render_click("delete", %{"secret" => secret})
+               |> follow_redirect(conn)
+
+      assert html =~ "Secret deleted successfully"
+      assert html =~ "Secrets"
+      refute html =~ "FOO_ENV_VAR"
+      assert html =~ "LIVEBOOK_PASSWORD"
+      assert html =~ "LIVEBOOK_SECRET_KEY_BASE"
+    end
   end
 
-  defp fly_bypass(app_id) do
-    bypass = Bypass.open()
-    Application.put_env(:livebook, :fly_graphql_endpoint, "http://localhost:#{bypass.port}")
-
+  defp fly_bypass(bypass, app_id, agent_pid) do
     Bypass.expect(bypass, "POST", "/", fn conn ->
       {:ok, body, conn} = Plug.Conn.read_body(conn)
+      body = Jason.decode!(body)
 
       response =
-        case Jason.decode!(body) do
-          %{"variables" => %{"appId" => ^app_id}} -> fetch_app_response(app_id)
-          %{"variables" => %{}} -> fetch_apps_response(app_id)
+        cond do
+          body["query"] =~ "setSecrets" ->
+            put_secrets_response()
+
+          body["query"] =~ "unsetSecrets" ->
+            delete_secrets_response()
+
+          true ->
+            Agent.get(agent_pid, fn
+              %{fun: fun, type: type} -> fun.(app_id, type)
+              %{fun: fun} -> fun.()
+            end)
         end
+
+      # cond do
+      #  body["query"] =~ "setSecrets" -> put_secrets_response()
+      #  body["query"] =~ "unsetSecrets" -> delete_secrets_response()
+      #  body["query"] =~ "secrets" -> fetch_secrets_response()
+      #  body["query"] =~ "app" -> fetch_app_response(app_id)
+      # end
 
       conn
       |> Plug.Conn.put_resp_content_type("application/json")
@@ -76,29 +233,87 @@ defmodule LivebookWeb.Hub.EditLiveTest do
     end)
   end
 
-  defp fetch_apps_response(app_id) do
-    app = %{
-      "id" => app_id,
-      "organization" => %{
-        "id" => "l3soyvjmvtmwtl6l2drnbfuvltipprge",
-        "name" => "Foo Bar",
-        "type" => "PERSONAL"
-      }
-    }
-
-    %{"data" => %{"apps" => %{"nodes" => [app]}}}
-  end
-
-  defp fetch_app_response(app_id) do
+  defp fetch_app_response(app_id, type) do
     app = %{
       "id" => app_id,
       "name" => app_id,
       "hostname" => app_id <> ".fly.dev",
       "platformVersion" => "nomad",
       "deployed" => true,
-      "status" => "running"
+      "status" => "running",
+      "secrets" => secrets(type)
     }
 
     %{"data" => %{"app" => app}}
+  end
+
+  defp secrets(:mount) do
+    [
+      %{
+        "createdAt" => to_string(DateTime.utc_now()),
+        "digest" => to_string(Livebook.Utils.random_cookie()),
+        "id" => "123",
+        "name" => "LIVEBOOK_PASSWORD"
+      },
+      %{
+        "createdAt" => to_string(DateTime.utc_now()),
+        "digest" => to_string(Livebook.Utils.random_cookie()),
+        "id" => "456",
+        "name" => "LIVEBOOK_SECRET_KEY_BASE"
+      }
+    ]
+  end
+
+  defp secrets(:add) do
+    [
+      %{
+        "createdAt" => to_string(DateTime.utc_now()),
+        "digest" => to_string(Livebook.Utils.random_cookie()),
+        "id" => "789",
+        "name" => "FOO_ENV_VAR"
+      },
+      %{
+        "createdAt" => to_string(DateTime.utc_now()),
+        "digest" => to_string(Livebook.Utils.random_cookie()),
+        "id" => "123",
+        "name" => "LIVEBOOK_PASSWORD"
+      },
+      %{
+        "createdAt" => to_string(DateTime.utc_now()),
+        "digest" => to_string(Livebook.Utils.random_cookie()),
+        "id" => "456",
+        "name" => "LIVEBOOK_SECRET_KEY_BASE"
+      }
+    ]
+  end
+
+  defp secrets(:foo) do
+    [
+      %{
+        "createdAt" => "2022-08-31 14:47:39.904338Z",
+        "digest" => to_string(Livebook.Utils.random_cookie()),
+        "id" => "123456789",
+        "name" => "FOO_ENV_VAR"
+      }
+    ]
+  end
+
+  defp secrets(:updated_foo) do
+    [
+      %{
+        "createdAt" => "2022-08-31 14:47:41.632669Z",
+        "digest" => to_string(Livebook.Utils.random_cookie()),
+        "id" => "123456789",
+        "name" => "FOO_ENV_VAR"
+      }
+    ]
+  end
+
+  defp put_secrets_response do
+    %{"data" => %{"setSecrets" => %{"app" => %{"secrets" => secrets(:add)}}}}
+  end
+
+  defp delete_secrets_response do
+    %{"data" => %{"unsetSecrets" => %{"app" => %{"secrets" => secrets(:mount)}}}}
   end
 end
