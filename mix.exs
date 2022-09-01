0410275071a237b06c6bd7e8cecd6d5e79c5a08e
Alexandre de Souza <alexandre@aledsz.com.br>
2022-09-01 13:12:18 -0300

    Update mixfile elixir version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Livebook.MixProject do
   use Mix.Project
 
-  @elixir_requirement "~> 1.14-rc.0"
+  @elixir_requirement "~> 1.14-rc.1"
   @version "0.6.3"
   @description "Interactive and collaborative code notebooks - made with Phoenix LiveView"
 
