ff0c81e3624a4280aa77b49e1bbea817f520d8e2
Alexandre de Souza <alexandre@aledsz.com.br>
2022-09-06 16:12:25 -0300

    Enforce upcased keys

--- lib/livebook/settings/environment_variable.ex
+++ lib/livebook/settings/environment_variable.ex
@@ -18,7 +18,15 @@ defmodule Livebook.Settings.EnvironmentVariable do
     |> cast(attrs, [:key, :value])
     |> validate_format(:key, ~r/^(?![l|L][b|B]_)\w+$/)
     |> validate_required([:key, :value])
+    |> upcase_key()
     |> add_id()
+  end
+
+  defp upcase_key(changeset) do
+    case get_field(changeset, :key) do
+      nil -> changeset
+      key -> put_change(changeset, :key, String.upcase(key))
+    end
   end
 
   defp add_id(changeset) do
