1639435024a98e2064cc01d86e2f82ef207dd128
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-10-07 12:19:20 +0200

    Update references to kino packages (#1467)

--- lib/livebook/runtime/elixir_standalone.ex
+++ lib/livebook/runtime/elixir_standalone.ex
@@ -17,8 +17,8 @@ defmodule Livebook.Runtime.ElixirStandalone do
           server_pid: pid() | nil
         }
 
-  kino_vega_lite = %{name: "kino_vega_lite", dependency: {:kino_vega_lite, "~> 0.1.1"}}
-  kino_db = %{name: "kino_db", dependency: {:kino_db, "~> 0.1.2"}}
+  kino_vega_lite = %{name: "kino_vega_lite", dependency: {:kino_vega_lite, "~> 0.1.4"}}
+  kino_db = %{name: "kino_db", dependency: {:kino_db, "~> 0.2.0"}}
   kino_maplibre = %{name: "kino_maplibre", dependency: {:kino_maplibre, "~> 0.1.3"}}
 
   @extra_smart_cell_definitions [
