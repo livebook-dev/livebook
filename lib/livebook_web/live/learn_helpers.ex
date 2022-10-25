1345c959ce82d4afcba749a165e9e5325e952468
moogle19 <k.seidel@q1.eu>
2022-10-25 15:19:54 +0200

    Make notebook_card clickable

--- lib/livebook_web/live/learn_helpers.ex
+++ lib/livebook_web/live/learn_helpers.ex
@@ -8,25 +8,22 @@ defmodule LivebookWeb.LearnHelpers do
   """
   def notebook_card(assigns) do
     ~H"""
-    <div class="flex flex-col">
-      <%= live_redirect to: Routes.learn_path(@socket, :notebook, @notebook_info.slug),
-            class: "flex items-center justify-center p-6 border-2 border-gray-100 rounded-t-2xl h-[150px]" do %>
+    <%= live_redirect to: Routes.learn_path(@socket, :notebook, @notebook_info.slug),
+            class: "flex flex-col border-2 border-gray-100 hover:border-gray-200 rounded-2xl" do %>
+      <div class="flex items-center justify-center p-6 border-b-2 border-gray-100 rounded-t-2xl h-[150px]">
         <img
           src={@notebook_info.details.cover_url}
           class="max-h-full max-w-[75%]"
           alt={"#{@notebook_info.title} logo"}
         />
-      <% end %>
+      </div>
       <div class="px-6 py-4 bg-gray-100 rounded-b-2xl grow">
-        <%= live_redirect(@notebook_info.title,
-          to: Routes.learn_path(@socket, :notebook, @notebook_info.slug),
-          class: "text-gray-800 font-semibold cursor-pointer"
-        ) %>
+        <span class="text-gray-800 font-semibold"><%= @notebook_info.title %></span>
         <p class="mt-2 text-sm text-gray-600">
           <%= @notebook_info.details.description %>
         </p>
       </div>
-    </div>
+    <% end %>
     """
   end
 end
