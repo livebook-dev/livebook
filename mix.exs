861bb9a5d76d5c63a70e714b595d33b40c1a2a9c
Alexandre de Souza <alexandre@aledsz.com.br>
2022-11-01 15:28:18 -0300

    Remove protobuf from Livebook

--- mix.exs
+++ mix.exs
@@ -102,7 +102,6 @@ defmodule Livebook.MixProject do
       {:ecto, "~> 3.9.0"},
       {:phoenix_ecto, "~> 4.4.0"},
       {:mint_web_socket, "~> 1.0.0"},
-      {:protobuf, "~> 0.8.0"},
       {:livebook_proto, path: "proto"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:floki, ">= 0.27.0", only: :test},
