ca80ee6b77b003ae31839b18c9c16187285d8539
Alexandre de Souza <alexandre@aledsz.com.br>
2022-12-13 14:47:28 -0300

    Add WebSocket server tests with `send_message/2`

--- test/livebook/web_socket/server_test.exs
+++ test/livebook/web_socket/server_test.exs
@@ -1,7 +1,12 @@
 defmodule Livebook.WebSocket.ServerTest do
   use Livebook.EnterpriseIntegrationCase, async: true
 
+  @app_version Mix.Project.config()[:version]
+
   alias Livebook.WebSocket.Server
+  alias Livebook.WebSocket.Client.Response
+
+  alias LivebookProto.Request
 
   describe "connect/2" do
     test "successfully authenticates the websocket connection", %{url: url, token: token} do
@@ -42,6 +47,30 @@ defmodule Livebook.WebSocket.ServerTest do
       assert error =~ "could not get the token from the connection"
 
       assert Server.disconnect(pid) == :ok
+    end
+  end
+
+  describe "send_message/2" do
+    setup %{url: url, token: token} do
+      headers = [{"X-Auth-Token", token}]
+
+      {:ok, pid} = Server.start_link()
+      {:ok, :connected} = Server.connect(pid, url, headers)
+
+      on_exit(fn -> Server.disconnect(pid) end)
+
+      {:ok, pid: pid}
+    end
+
+    test "successfully sends a session message", %{pid: pid, user: %{id: id, email: email}} do
+      session_request = LivebookProto.SessionRequest.new!(app_version: @app_version)
+      request = Request.new!(type: {:session, session_request})
+      frame = {:binary, Request.encode(request)}
+
+      assert Server.send_message(pid, frame) == :ok
+
+      assert_receive {:ok, %Response{body: body, status: nil, headers: nil}}
+      assert {:session, %{id: _, user: %{id: ^id, email: ^email}}} = body
     end
   end
 end
