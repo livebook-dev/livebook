3480b5ef800fdae3859ecdfe5350a4eb15e3ab7e
Alexandre de Souza <alexandre@aledsz.com.br>
2022-12-16 17:30:12 -0300

    Add WebSocket PubSub

--- lib/livebook/web_socket.ex
+++ lib/livebook/web_socket.ex
@@ -101,4 +101,38 @@ defmodule Livebook.WebSocket do
   defp handle_receive({:error, reason}, ref) do
     {:error, %Connection{ref: ref}, reason}
   end
+
+  @doc """
+  Subscribe to WebSocket Server events.
+
+  ## Messages
+
+    * `{:ok, pid, random_id, :connected}`
+    * `{:ok, pid, random_id, %Livebook.WebSocket.Response{}}`
+    * `{:error, pid, random_id, %Livebook.WebSocket.Response{}}`
+    * `{:error, pid, random_id, reason}`
+
+  """
+  @spec subscribe() :: :ok | {:error, {:already_registered, pid()}}
+  def subscribe do
+    Phoenix.PubSub.subscribe(Livebook.PubSub, "websocket:clients")
+  end
+
+  @doc """
+  Unsubscribes from `subscribe/0`.
+  """
+  @spec unsubscribe() :: :ok
+  def unsubscribe do
+    Phoenix.PubSub.unsubscribe(Livebook.PubSub, "websocket:clients")
+  end
+
+  @doc """
+  Notifies interested processes about WebSocket Server messages.
+
+  Broadcasts the given message under the `"websocket:clients"` topic.
+  """
+  @spec broadcast_message(any()) :: :ok
+  def broadcast_message(message) do
+    Phoenix.PubSub.broadcast(Livebook.PubSub, "websocket:clients", message)
+  end
 end
