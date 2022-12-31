50e0133c3467d348152cb08581ec2df299594b5b
Wojtek Mach <wojtek@wojtekmach.pl>
2022-12-31 12:52:34 +0100

    elixirkit/demo/rel/swift: Clean up

--- elixirkit/demo/rel/swift/Package.swift
+++ elixirkit/demo/rel/swift/Package.swift
@@ -1,22 +1,14 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Demo",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Demo",
-            dependencies: []),
-        .testTarget(
-            name: "DemoTests",
-            dependencies: ["Demo"]),
+            dependencies: []
+        )
     ]
 )
