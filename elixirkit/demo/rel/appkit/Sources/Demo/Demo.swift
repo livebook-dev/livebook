df630c1180afc6617585315789e2c932a540326e
Wojtek Mach <wojtek@wojtekmach.pl>
2022-12-31 13:16:03 +0100

    elixirkit/demo/rel/appkit: Initial UI

--- elixirkit/demo/rel/appkit/Sources/Demo/Demo.swift
+++ elixirkit/demo/rel/appkit/Sources/Demo/Demo.swift
@@ -1,8 +1,40 @@
+import AppKit
+
 @main
 public struct Demo {
-    public private(set) var text = "Hello, World!"
-
     public static func main() {
-        print(Demo().text)
+        let app = NSApplication.shared
+        let delegate = AppDelegate()
+        app.delegate = delegate
+        app.run()
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    private var window : NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let menuItemOne = NSMenuItem()
+        menuItemOne.submenu = NSMenu(title: "Demo")
+        menuItemOne.submenu?.items = [
+            NSMenuItem(title: "Quit Demo", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        ]
+        let menu = NSMenu()
+        menu.items = [menuItemOne]
+        NSApp.mainMenu = menu
+
+        window = NSWindow(contentRect: NSMakeRect(0, -1000, 200, 200),
+                          styleMask: [.titled, .closable],
+                          backing: .buffered,
+                          defer: true)
+        window.orderFrontRegardless()
+        window.title = "Demo"
+
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ app: NSApplication) -> Bool {
+        return true
     }
 }
