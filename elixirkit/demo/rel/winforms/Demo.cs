dac72088b45bc7013f5921b61aeeafbfe266d792
Wojtek Mach <wojtek@wojtekmach.pl>
2022-12-31 13:09:13 +0100

    elixirkit/demo/rel/winforms: Clean up

--- elixirkit/demo/rel/winforms/Demo.cs
+++ elixirkit/demo/rel/winforms/Demo.cs
@@ -1,16 +1,26 @@
-﻿namespace Demo;
+namespace Demo;
 
-partial class Form1
+static class DemoMain
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new DemoForm());
+    }
+}
+
+class DemoForm : Form
+{
+    public DemoForm()
+    {
+        InitializeComponent();
+    }
+
+    // WinForms boilerplate below.
+
     private System.ComponentModel.IContainer components = null;
 
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
     protected override void Dispose(bool disposing)
     {
         if (disposing && (components != null))
@@ -22,16 +32,12 @@ partial class Form1
 
     #region Windows Form Designer generated code
 
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        this.Text = "Demo";
     }
 
     #endregion
--- elixirkit/demo/rel/winforms/Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace Demo;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-}
