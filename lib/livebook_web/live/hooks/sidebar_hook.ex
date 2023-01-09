3b4686983770871c261b2e2244c7cc2f3c12f12b
Alexandre de Souza <alexandre@aledsz.com.br>
2023-01-09 17:03:23 -0300

    Remove old implementation

--- lib/livebook_web/live/hooks/sidebar_hook.ex
+++ lib/livebook_web/live/hooks/sidebar_hook.ex
@@ -4,9 +4,6 @@ defmodule LivebookWeb.SidebarHook do
   import Phoenix.Component
   import Phoenix.LiveView
 
-  alias Livebook.Hubs.Enterprise
-  alias Livebook.Hubs.EnterpriseClient
-
   def on_mount(:default, _params, _session, socket) do
     if connected?(socket) do
       Livebook.Hubs.subscribe()
@@ -20,11 +17,12 @@ defmodule LivebookWeb.SidebarHook do
       |> attach_hook(:hubs, :handle_info, &handle_info/2)
       |> attach_hook(:shutdown, :handle_event, &handle_event/3)
 
-    {:cont, assign(socket, connected_hubs: connect_enterprise_hubs(hubs))}
+    {:cont, socket}
   end
 
   defp handle_info({:hubs_metadata_changed, hubs}, socket) do
-    {:halt, assign(socket, saved_hubs: hubs, connected_hubs: connect_enterprise_hubs(hubs))}
+    Livebook.Hubs.connect_hubs()
+    {:halt, assign(socket, saved_hubs: hubs)}
   end
 
   defp handle_info(_event, socket), do: {:cont, socket}
@@ -42,26 +40,4 @@ defmodule LivebookWeb.SidebarHook do
   end
 
   defp handle_event(_event, _params, socket), do: {:cont, socket}
-
-  # TODO: Move Hub connection life-cycle elsewhere
-  @supervisor Livebook.HubsSupervisor
-  @registry Livebook.HubsRegistry
-
-  defp connect_enterprise_hubs(hubs) do
-    for %{provider: %Enterprise{} = enterprise} <- hubs do
-      pid =
-        case Registry.lookup(@registry, enterprise.url) do
-          [{pid, _}] ->
-            pid
-
-          [] ->
-            case DynamicSupervisor.start_child(@supervisor, {EnterpriseClient, enterprise}) do
-              {:ok, pid} -> pid
-              {:error, {:already_started, pid}} -> pid
-            end
-        end
-
-      %{hub: enterprise, pid: pid}
-    end
-  end
 end
