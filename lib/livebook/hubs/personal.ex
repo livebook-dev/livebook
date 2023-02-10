3518111cd42ce33d5f3ec1f19b1331fb26aa47ff
Alexandre de Souza <alexandre@aledsz.com.br>
2023-02-10 18:46:33 -0300

    Transform Personal hub into an Ecto Schema

--- lib/livebook/hubs/personal.ex
+++ lib/livebook/hubs/personal.ex
@@ -1,7 +1,56 @@
 defmodule Livebook.Hubs.Personal do
   @moduledoc false
 
-  defstruct [:id, :hub_name, :hub_emoji]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Livebook.Hubs
+
+  @type t :: %__MODULE__{
+          id: String.t() | nil,
+          hub_name: String.t() | nil,
+          hub_emoji: String.t() | nil
+        }
+
+  embedded_schema do
+    field :hub_name, :string
+    field :hub_emoji, :string
+  end
+
+  @fields ~w(hub_name hub_emoji)a
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking hub changes.
+  """
+  @spec change_hub(t(), map()) :: Ecto.Changeset.t()
+  def change_hub(%__MODULE__{} = personal, attrs \\ %{}) do
+    personal
+    |> changeset(attrs)
+    |> Map.put(:action, :validate)
+  end
+
+  @doc """
+  Updates a Hub.
+
+  With success, notifies interested processes about hub metadatas data change.
+  Otherwise, it will return an error tuple with changeset.
+  """
+  @spec update_hub(t(), map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def update_hub(%__MODULE__{} = personal, attrs) do
+    changeset = changeset(personal, attrs)
+
+    with {:ok, struct} <- apply_action(changeset, :update) do
+      Hubs.save_hub(struct)
+      {:ok, struct}
+    end
+  end
+
+  defp changeset(fly, attrs) do
+    fly
+    |> cast(attrs, @fields)
+    |> validate_required(@fields)
+    |> put_change(:id, "personal-hub")
+  end
 end
 
 defimpl Livebook.Hubs.Provider, for: Livebook.Hubs.Personal do
