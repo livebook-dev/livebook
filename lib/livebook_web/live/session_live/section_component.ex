179894903dddeae2bc9e8e5a366cb06f78b30555
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-03-13 14:47:32 +0100

    Format

--- lib/livebook_web/live/session_live/section_component.ex
+++ lib/livebook_web/live/session_live/section_component.ex
@@ -152,7 +152,10 @@ defmodule LivebookWeb.SessionLive.SectionComponent do
         <span class="leading-none">from ”<%= @section_view.parent.name %>”</span>
       </h3>
 
-      <h3 class="mt-2 text-sm text-gray-500 cursor-default select-none" data-el-section-subheadline-collapsed>
+      <h3
+        class="mt-2 text-sm text-gray-500 cursor-default select-none"
+        data-el-section-subheadline-collapsed
+      >
         <%= pluralize(length(@section_view.cell_views), "cell", "cells") %> collapsed
       </h3>
       <div class="container" data-el-section-content>
