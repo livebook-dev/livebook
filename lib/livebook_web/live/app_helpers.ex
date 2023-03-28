0014b492a3b4e2d518ba4a815eb2eda3cccb7d65
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-03-29 00:28:18 +0200

    Show status for non-registered apps

--- lib/livebook_web/live/app_helpers.ex
+++ lib/livebook_web/live/app_helpers.ex
@@ -5,41 +5,42 @@ defmodule LivebookWeb.AppHelpers do
   Renders app status with indicator.
   """
   attr :status, :atom, required: true
+  attr :show_label, :boolean, default: true
 
   def app_status(%{status: :booting} = assigns) do
     ~H"""
-    <.app_status_indicator text="Booting" variant={:progressing} />
+    <.app_status_indicator text={@show_label && "Booting"} variant={:progressing} />
     """
   end
 
   def app_status(%{status: :running} = assigns) do
     ~H"""
-    <.app_status_indicator text="Running" variant={:success} />
+    <.app_status_indicator text={@show_label && "Running"} variant={:success} />
     """
   end
 
   def app_status(%{status: :error} = assigns) do
     ~H"""
-    <.app_status_indicator text="Error" variant={:error} />
+    <.app_status_indicator text={@show_label && "Error"} variant={:error} />
     """
   end
 
   def app_status(%{status: :shutting_down} = assigns) do
     ~H"""
-    <.app_status_indicator text="Shutting down" variant={:inactive} />
+    <.app_status_indicator text={@show_label && "Shutting down"} variant={:inactive} />
     """
   end
 
   def app_status(%{status: :stopped} = assigns) do
     ~H"""
-    <.app_status_indicator text="Stopped" variant={:inactive} />
+    <.app_status_indicator text={@show_label && "Stopped"} variant={:inactive} />
     """
   end
 
   defp app_status_indicator(assigns) do
     ~H"""
     <div class="flex items-center space-x-2">
-      <div><%= @text %></div>
+      <div :if={@text}><%= @text %></div>
       <.status_indicator variant={@variant} />
     </div>
     """
--- lib/livebook_web/live/auth_app_list_live.ex
+++ lib/livebook_web/live/auth_app_list_live.ex
@@ -1,6 +1,7 @@
 defmodule LivebookWeb.AuthAppListLive do
   use LivebookWeb, :live_view
 
+  import LivebookWeb.AppHelpers
   import LivebookWeb.SessionHelpers
 
   @impl true
@@ -14,7 +15,6 @@ defmodule LivebookWeb.AuthAppListLive do
     {:ok,
      assign(socket,
        sessions: sessions,
-       booting_count: booting_count(sessions),
        empty_apps_path?: Livebook.Apps.empty_apps_path?()
      ), layout: false}
   end
@@ -30,14 +30,20 @@ defmodule LivebookWeb.AuthAppListLive do
         <.link
           :for={session <- visible_sessions(@sessions)}
           navigate={~p"/apps/#{session.app_info.slug}"}
-          class="px-4 py-3 border border-gray-200 rounded-xl text-gray-800 pointer hover:bg-gray-50 flex justify-between"
+          class={[
+            "px-4 py-3 border border-gray-200 rounded-xl text-gray-800 pointer hover:bg-gray-50 flex justify-between",
+            not session.app_info.registered && "pointer-events-none"
+          ]}
         >
           <span class="font-semibold"><%= session.notebook_name %></span>
-          <.remix_icon icon="arrow-right-line" class="" />
+          <%= if session.app_info.registered do %>
+            <.remix_icon icon="arrow-right-line" class="" />
+          <% else %>
+            <div class="mr-0.5 flex">
+              <.app_status status={session.app_info.status} show_label={false} />
+            </div>
+          <% end %>
         </.link>
-      </div>
-      <div :if={@booting_count > 0} class="mt-2 text-gray-600 text-sm">
-        <%= pluralize(@booting_count, "app", "apps") %> booting
       </div>
       <div :if={@empty_apps_path?} class="mt-5 text-gray-600">
         <div>
@@ -68,19 +74,14 @@ defmodule LivebookWeb.AuthAppListLive do
   @impl true
   def handle_info({type, session} = event, socket)
       when type in [:session_created, :session_updated, :session_closed] and session.mode == :app do
-    sessions = update_session_list(socket.assigns.sessions, event)
-    {:noreply, assign(socket, sessions: sessions, booting_count: booting_count(sessions))}
+    {:noreply, update(socket, :sessions, &update_session_list(&1, event))}
   end
 
   def handle_info(_message, socket), do: {:noreply, socket}
 
   defp visible_sessions(sessions) do
     sessions
-    |> Enum.filter(&(&1.app_info.public? and &1.app_info.registered))
+    |> Enum.filter(& &1.app_info.public?)
     |> Enum.sort_by(& &1.notebook_name)
-  end
-
-  defp booting_count(sessions) do
-    Enum.count(sessions, &(&1.app_info.public? and &1.app_info.status == :booting))
   end
 end
