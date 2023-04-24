4c9d502369985f866503cae6c31f55806605c867
Alexandre de Souza <alexandre@aledsz.com.br>
2023-04-24 17:09:30 -0300

    Fix `get_secrets/2` when no GenServer connected

--- lib/livebook/hubs/enterprise_client.ex
+++ lib/livebook/hubs/enterprise_client.ex
@@ -55,6 +55,8 @@ defmodule Livebook.Hubs.EnterpriseClient do
   @spec get_secrets(String.t()) :: list(Secret.t())
   def get_secrets(id) do
     GenServer.call(registry_name(id), :get_secrets)
+  catch
+    :exit, _ -> []
   end
 
   @doc """
