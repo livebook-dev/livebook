b6b68101f7446a6608e87ddd029e63a436ad5207
Alexandre de Souza <alexandre@aledsz.com.br>
2023-05-29 17:02:05 -0300

    Move to Teams namespace

---
+++ lib/livebook/teams/connection.ex
@@ -0,0 +1,119 @@
+defmodule Livebook.Teams.Connection do
+  @moduledoc false
+
+  @behaviour :gen_statem
+
+  require Logger
+
+  alias Livebook.WebSocket
+  alias Livebook.Teams.WebSocket
+
+  @backoff 5_000
+  @no_state :no_state
+  @loop_ping_delay 5_000
+
+  defstruct [:listener, :headers, :http_conn, :websocket, :ref]
+
+  @doc """
+  Starts a new WebSocket connection with given headers.
+  """
+  @spec start_link(pid(), Mint.Types.headers()) :: :gen_statem.start_ret()
+  def start_link(listener, headers \\ []) do
+    :gen_statem.start_link(__MODULE__, {listener, headers}, [])
+  end
+
+  ## gen_statem callbacks
+
+  @impl true
+  def callback_mode(), do: :handle_event_function
+
+  @impl true
+  def init({listener, headers}) do
+    data = %__MODULE__{listener: listener, headers: headers}
+    {:ok, @no_state, data, {:next_event, :internal, :connect}}
+  end
+
+  @impl true
+  def handle_event(event_type, event_data, state, data)
+
+  def handle_event(:internal, :connect, @no_state, %__MODULE__{} = data) do
+    case WebSocket.connect(data.headers) do
+      {:ok, conn, websocket, ref} ->
+        send(data.listener, {:connect, :ok, :connected})
+        send(self(), {:loop_ping, ref})
+
+        {:keep_state, %__MODULE__{data | http_conn: conn, ref: ref, websocket: websocket}}
+
+      {:transport_error, reason} ->
+        send(data.listener, {:connect, :error, reason})
+        {:keep_state_and_data, {{:timeout, :backoff}, @backoff, nil}}
+
+      {:server_error, error} ->
+        send(data.listener, {:connect, :error, error})
+
+        {:keep_state_and_data, {{:timeout, :reconnect}, @backoff, nil}}
+    end
+  end
+
+  def handle_event({:timeout, :backoff}, nil, _state, _data) do
+    {:keep_state_and_data, {:next_event, :internal, :connect}}
+  end
+
+  def handle_event({:timeout, :reconnect}, nil, _state, _data) do
+    {:keep_state_and_data, {:next_event, :internal, :connect}}
+  end
+
+  def handle_event(:info, {:loop_ping, ref}, @no_state, %__MODULE__{ref: ref} = data) do
+    case WebSocket.send(data.http_conn, data.websocket, data.ref, :ping) do
+      {:ok, conn, websocket} ->
+        Process.send_after(self(), {:loop_ping, data.ref}, @loop_ping_delay)
+        {:keep_state, %__MODULE__{data | http_conn: conn, websocket: websocket}}
+
+      {:error, conn, websocket, _reason} ->
+        {:keep_state, %__MODULE__{data | http_conn: conn, websocket: websocket}}
+    end
+  end
+
+  def handle_event(:info, {:loop_ping, _another_ref}, @no_state, _data) do
+    :keep_state_and_data
+  end
+
+  def handle_event(:info, {:tcp_closed, _port} = message, @no_state, %__MODULE__{} = data) do
+    handle_websocket_message(message, data)
+  end
+
+  def handle_event(:info, {:tcp, _port, _data} = message, @no_state, %__MODULE__{} = data) do
+    handle_websocket_message(message, data)
+  end
+
+  def handle_event(:info, _message, @no_state, _data) do
+    :keep_state_and_data
+  end
+
+  # Private
+
+  defp handle_websocket_message(message, %__MODULE__{} = data) do
+    case WebSocket.receive(data.http_conn, data.ref, data.websocket, message) do
+      {:ok, conn, websocket, binaries} ->
+        data = %__MODULE__{data | http_conn: conn, websocket: websocket}
+        send_received(binaries, data)
+
+        {:keep_state, data}
+
+      {:server_error, conn, websocket, reason} ->
+        send(data.listener, {:connect, :error, reason})
+        data = %__MODULE__{data | http_conn: conn, websocket: websocket}
+
+        {:keep_state, data, {:next_event, :internal, :connect}}
+    end
+  end
+
+  defp send_received([], _data), do: :ok
+
+  defp send_received([_ | _] = binaries, data) do
+    for binary <- binaries do
+      %{type: {name, struct}} = LivebookProto.Event.decode(binary)
+      send(data.listener, {:event, name, struct})
+    end
+  end
+end
