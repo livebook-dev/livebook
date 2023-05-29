c13b6ee16cf641e9c4774a75c4926900e4a058fc
Alexandre de Souza <alexandre@aledsz.com.br>
2023-05-29 08:42:59 -0300

    Update LivebookProto

--- proto/lib/livebook_proto/handshake_response.pb.ex
+++
@@ -1,8 +0,0 @@
-defmodule LivebookProto.HandshakeResponse do
-  @moduledoc false
-  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
-
-  field :id, 1, type: :string
-  field :name, 2, type: :string
-  field :user, 3, type: LivebookProto.User
-end
