f22638795863108c3e6bf78a22ecfc7a98b5a353
Wojtek Mach <wojtek@wojtekmach.pl>
2023-05-29 16:21:29 +0200

    Update lib/livebook_web/live/session_live/attached_live.ex

--- lib/livebook_web/live/session_live/attached_live.ex
+++ lib/livebook_web/live/session_live/attached_live.ex
@@ -38,7 +38,7 @@ defmodule LivebookWeb.SessionLive.AttachedLive do
       </p>
       <div class="text-gray-700 markdown">
         <%= if longname = Livebook.Config.longname() do %>
-          <pre><code>iex --name test@<%= longname %> --cookie mycookie</code> -S mix</pre>
+          <pre><code>iex --name test@<%= longname %> --cookie mycookie -S mix</code></pre>
         <% else %>
           <pre><code>iex --sname test --cookie mycookie -S mix</code></pre>
         <% end %>
