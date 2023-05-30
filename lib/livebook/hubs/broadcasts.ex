93b1117fe369b9a117101d44f9e1d6a9208be88d
Alexandre de Souza <alexandre@aledsz.com.br>
2023-05-30 12:33:31 -0300

    Improve docs

--- lib/livebook/hubs/broadcasts.ex
+++ lib/livebook/hubs/broadcasts.ex
@@ -10,7 +10,7 @@ defmodule Livebook.Hubs.Broadcasts do
   @secrets_topic "hubs:secrets"
 
   @doc """
-  Broadcasts under `hubs:crud` topic when hubs changed.
+  Broadcasts under `#{@crud_topic}` topic when hubs changed.
   """
   @spec hub_changed() :: broadcast()
   def hub_changed do
@@ -18,7 +18,7 @@ defmodule Livebook.Hubs.Broadcasts do
   end
 
   @doc """
-  Broadcasts under `hubs:connection` topic when hub connected.
+  Broadcasts under `#{@connection_topic}` topic when hub connected.
   """
   @spec hub_connected() :: broadcast()
   def hub_connected do
@@ -26,7 +26,7 @@ defmodule Livebook.Hubs.Broadcasts do
   end
 
   @doc """
-  Broadcasts under `hubs:connection` topic when hub disconnected.
+  Broadcasts under `#{@connection_topic}` topic when hub disconnected.
   """
   @spec hub_disconnected() :: broadcast()
   def hub_disconnected do
@@ -34,7 +34,7 @@ defmodule Livebook.Hubs.Broadcasts do
   end
 
   @doc """
-  Broadcasts under `hubs:connection` topic when hub received a connection error.
+  Broadcasts under `#{@connection_topic}` topic when hub received a connection error.
   """
   @spec hub_connection_failed(String.t()) :: broadcast()
   def hub_connection_failed(reason) when is_binary(reason) do
@@ -42,7 +42,7 @@ defmodule Livebook.Hubs.Broadcasts do
   end
 
   @doc """
-  Broadcasts under `hubs:secrets` topic when hub received a new secret.
+  Broadcasts under `#{@secrets_topic}` topic when hub received a new secret.
   """
   @spec secret_created(Secret.t()) :: broadcast()
   def secret_created(%Secret{} = secret) do
@@ -50,7 +50,7 @@ defmodule Livebook.Hubs.Broadcasts do
   end
 
   @doc """
-  Broadcasts under `hubs:secrets` topic when hub received an updated secret.
+  Broadcasts under `#{@secrets_topic}` topic when hub received an updated secret.
   """
   @spec secret_updated(Secret.t()) :: broadcast()
   def secret_updated(%Secret{} = secret) do
@@ -58,7 +58,7 @@ defmodule Livebook.Hubs.Broadcasts do
   end
 
   @doc """
-  Broadcasts under `hubs:secrets` topic when hub received a deleted secret.
+  Broadcasts under `#{@secrets_topic}` topic when hub received a deleted secret.
   """
   @spec secret_deleted(Secret.t()) :: broadcast()
   def secret_deleted(%Secret{} = secret) do
