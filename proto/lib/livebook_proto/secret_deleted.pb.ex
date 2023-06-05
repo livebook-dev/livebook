3721bc0f335395aa82bf70631d24bb49b288efe3
Alexandre de Souza <alexandre@aledsz.com.br>
2023-06-05 18:27:19 -0300

    Remove value field from SecretDeleted event

--- proto/lib/livebook_proto/secret_deleted.pb.ex
+++ proto/lib/livebook_proto/secret_deleted.pb.ex
@@ -3,5 +3,4 @@ defmodule LivebookProto.SecretDeleted do
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
   field :name, 1, type: :string
-  field :value, 2, type: :string
 end
