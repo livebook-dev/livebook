1ab4935d314c69ba7a7887f9be0bc2e43bc380b4
Alexandre de Souza <alexandre@aledsz.com.br>
2023-06-14 17:58:16 -0300

    Add id to the New Secret button

--- lib/livebook_web/live/session_live/secrets_list_component.ex
+++ lib/livebook_web/live/session_live/secrets_list_component.ex
@@ -60,6 +60,7 @@ defmodule LivebookWeb.SessionLive.SecretsListComponent do
         </div>
 
         <.link
+          id="new-secret-button"
           patch={~p"/sessions/#{@session.id}/secrets"}
           class="inline-flex items-center justify-center p-8 py-1 mt-6 space-x-2 text-sm font-medium text-gray-500 border border-gray-400 border-dashed rounded-xl hover:bg-gray-100"
           role="button"
