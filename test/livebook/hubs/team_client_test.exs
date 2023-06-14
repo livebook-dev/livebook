53d3586947dd3e051ccf87318fec23d55f395020
Alexandre de Souza <alexandre@aledsz.com.br>
2023-06-14 15:02:17 -0300

    Add handle events tests

--- test/livebook/hubs/team_client_test.exs
+++ test/livebook/hubs/team_client_test.exs
@@ -6,7 +6,7 @@ defmodule Livebook.Hubs.TeamClientTest do
   @moduletag :capture_log
 
   setup do
-    Livebook.Hubs.subscribe([:connection])
+    Livebook.Hubs.subscribe([:connection, :secrets])
     :ok
   end
 
@@ -56,6 +56,50 @@ defmodule Livebook.Hubs.TeamClientTest do
                "#{team.hub_name}: Your session is out-of-date. Please re-join the organization."
 
       refute Livebook.Hubs.hub_exists?(team.id)
+    end
+  end
+
+  describe "handle events" do
+    test "receives the secret_created event", %{user: user, node: node} do
+      teams_org = build(:org)
+      teams_key = teams_org.teams_key
+      key_hash = Livebook.Teams.Org.key_hash(teams_org)
+
+      org = :erpc.call(node, Hub.Integration, :create_org, [])
+
+      org_key =
+        :erpc.call(node, Hub.Integration, :create_org_key, [[org: org, key_hash: key_hash]])
+
+      org_key_pair = :erpc.call(node, Hub.Integration, :create_org_key_pair, [[org: org]])
+      token = :erpc.call(node, Hub.Integration, :associate_user_with_org, [user, org])
+
+      team =
+        build(:team,
+          id: "team-#{org.name}",
+          hub_name: org.name,
+          user_id: user.id,
+          org_id: org.id,
+          org_key_id: org_key.id,
+          org_public_key: org_key_pair.public_key,
+          session_token: token,
+          teams_key: teams_key
+        )
+
+      id = team.id
+
+      refute TeamClient.connected?(team.id)
+
+      TeamClient.start_link(team)
+      assert_receive {:hub_connected, ^id}
+
+      secret = build(:secret, name: "FOO", value: "BAR")
+      assert Livebook.Teams.create_secret(team, secret) == :ok
+
+      # receives `{:event, :secret_created, secret_created}` event
+      # with the value decrypted
+      assert_receive {:secret_created, secret_created}, 8000
+      assert secret_created.name == secret.name
+      assert secret_created.value == secret.value
     end
   end
 end
--- test/livebook/teams/connection_test.exs
+++ test/livebook/teams/connection_test.exs
@@ -41,4 +41,40 @@ defmodule Livebook.Teams.ConnectionTest do
                       "Invalid request. Please re-join the organization and update Livebook if the issue persists."}
     end
   end
+
+  describe "handle events" do
+    test "receives the secret_created event", %{user: user, node: node} do
+      org = :erpc.call(node, Hub.Integration, :create_org, [])
+      org_key = :erpc.call(node, Hub.Integration, :create_org_key, [[org: org]])
+      token = :erpc.call(node, Hub.Integration, :associate_user_with_org, [user, org])
+
+      header = [
+        {"x-user", to_string(user.id)},
+        {"x-org", to_string(org.id)},
+        {"x-org-key", to_string(org_key.id)},
+        {"x-session-token", token}
+      ]
+
+      assert {:ok, _conn} = Connection.start_link(self(), header)
+      assert_receive :connected
+
+      # creates a new secret
+      hub =
+        build(:team,
+          user_id: user.id,
+          org_id: org.id,
+          org_key_id: org_key.id,
+          session_token: token
+        )
+
+      secret = build(:secret, name: "FOO", value: "BAR")
+      assert Livebook.Teams.create_secret(hub, secret) == :ok
+
+      # receives `{:event, :secret_created, secret_created}` event
+      # without decrypting the value
+      assert_receive {:event, :secret_created, secret_created}
+      assert secret_created.name == secret.name
+      refute secret_created.value == secret.value
+    end
+  end
 end
