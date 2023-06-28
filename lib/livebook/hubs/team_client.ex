97bc141c188f479306c3417d1246d8eb6e976572
Alexandre de Souza <alexandre@aledsz.com.br>
2023-06-28 15:48:41 -0300

    Fix `get_secrets/1` when there's no active server

--- lib/livebook/hubs/team_client.ex
+++ lib/livebook/hubs/team_client.ex
@@ -41,6 +41,8 @@ defmodule Livebook.Hubs.TeamClient do
   @spec get_secrets(String.t()) :: list(Secret.t())
   def get_secrets(id) do
     GenServer.call(registry_name(id), :get_secrets)
+  catch
+    :exit, _ -> []
   end
 
   @doc """
