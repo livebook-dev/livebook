f3917d11f7bdb6c5f3f9aa279414bb35d74a69d3
Cristine Guadelupe <cristineguadelupe@me.com>
2023-07-03 21:20:00 +0200

    Update lib/livebook_web/live/hooks/user_hook.ex
    Co-authored-by: José Valim <jose.valim@gmail.com>

--- lib/livebook_web/live/hooks/user_hook.ex
+++ lib/livebook_web/live/hooks/user_hook.ex
@@ -40,8 +40,10 @@ defmodule LivebookWeb.UserHook do
     attrs = connect_params["user_data"] || session["user_data"] || %{}
 
     attrs =
-      Map.merge(attrs, identity_data)
-      |> then(&if &1["email"] && !&1["name"], do: %{&1 | "name" => &1["email"]}, else: &1)
+      case Map.merge(attrs, identity_data) do
+        %{"name" => nil, "email" => email} = attrs -> %{attrs | "name" => email}
+        attrs -> attrs
+      end
 
     case Livebook.Users.update_user(user, attrs) do
       {:ok, user} -> user
