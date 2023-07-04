599c3fa854c5f0a2072c84e23245bf4572e0e65b
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-07-04 13:30:33 +0200

    Add option to disable frame placeholder

--- lib/livebook_web/live/output/frame_component.ex
+++ lib/livebook_web/live/output/frame_component.ex
@@ -75,7 +75,7 @@ defmodule LivebookWeb.Output.FrameComponent do
     ~H"""
     <div id={"frame-output-#{@id}"}>
       <%= if @output_count == 0 do %>
-        <div class="text-gray-300 p-4 rounded-lg border border-gray-200">
+        <div :if={@placeholder} class="text-gray-300 p-4 rounded-lg border border-gray-200">
           Nothing here...
         </div>
       <% else %>
