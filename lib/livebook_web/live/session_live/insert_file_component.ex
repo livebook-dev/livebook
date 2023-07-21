83834166d22c1d1a9c00be1759221e5a41cb401c
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-07-21 20:07:35 +0200

    Update modal spacing

--- lib/livebook_web/live/session_live/insert_file_component.ex
+++ lib/livebook_web/live/session_live/insert_file_component.ex
@@ -4,14 +4,14 @@ defmodule LivebookWeb.SessionLive.InsertFileComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="p-6 flex flex-col space-y-3">
+    <div class="p-6">
       <h3 class="text-2xl font-semibold text-gray-800">
         Insert file
       </h3>
-      <p class="text-gray-700">
+      <p class="mt-8 text-gray-700">
         What do you want to do with the file?
       </p>
-      <div class="w-full flex flex-col space-y-4">
+      <div class="mt-4 w-full flex flex-col space-y-4">
         <div
           :for={{handler, idx} <- Enum.with_index(@insert_file_metadata.handlers)}
           class="px-4 py-3 border border-gray-200 rounded-xl text-gray-800 pointer hover:bg-gray-50 cursor-pointer"
