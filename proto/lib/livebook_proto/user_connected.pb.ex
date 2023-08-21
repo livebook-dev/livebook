582bd06a51b27bcba268a3ab2d2f6f89af581d38
Alexandre de Souza <alexandre@aledsz.com.br>
2023-08-21 17:20:40 -0300

    Update `protoc_gen_elixir_version` to 0.12.0

--- proto/lib/livebook_proto/user_connected.pb.ex
+++ proto/lib/livebook_proto/user_connected.pb.ex
@@ -1,6 +1,5 @@
 defmodule LivebookProto.UserConnected do
-  @moduledoc false
-  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
 
   field :name, 1, type: :string
   field :secrets, 2, repeated: true, type: LivebookProto.Secret
