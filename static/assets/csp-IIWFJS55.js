874155db157b1f570b8c9378005fe37cbd4ed174
josevalim <josevalim@users.noreply.github.com>
2023-08-22 07:09:45 +0000

    Update assets

---
+++ static/assets/csp-IIWFJS55.js
@@ -0,0 +1,11 @@
+import{e as t}from"./chunk-3X664NSF.js";var e,s,r=t(()=>{e={brackets:[],autoClosingPairs:[],surroundingPairs:[]},s={keywords:[],typeKeywords:[],tokenPostfix:".csp",operators:[],symbols:/[=><!~?:&|+\-*\/\^%]+/,escapes:/\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,tokenizer:{root:[[/child-src/,"string.quote"],[/connect-src/,"string.quote"],[/default-src/,"string.quote"],[/font-src/,"string.quote"],[/frame-src/,"string.quote"],[/img-src/,"string.quote"],[/manifest-src/,"string.quote"],[/media-src/,"string.quote"],[/object-src/,"string.quote"],[/script-src/,"string.quote"],[/style-src/,"string.quote"],[/worker-src/,"string.quote"],[/base-uri/,"string.quote"],[/plugin-types/,"string.quote"],[/sandbox/,"string.quote"],[/disown-opener/,"string.quote"],[/form-action/,"string.quote"],[/frame-ancestors/,"string.quote"],[/report-uri/,"string.quote"],[/report-to/,"string.quote"],[/upgrade-insecure-requests/,"string.quote"],[/block-all-mixed-content/,"string.quote"],[/require-sri-for/,"string.quote"],[/reflected-xss/,"string.quote"],[/referrer/,"string.quote"],[/policy-uri/,"string.quote"],[/'self'/,"string.quote"],[/'unsafe-inline'/,"string.quote"],[/'unsafe-eval'/,"string.quote"],[/'strict-dynamic'/,"string.quote"],[/'unsafe-hashed-attributes'/,"string.quote"]]}}});r();export{e as conf,s as language};
+/*! Bundled license information:
+
+monaco-editor/esm/vs/basic-languages/csp/csp.js:
+  (*!-----------------------------------------------------------------------------
+   * Copyright (c) Microsoft Corporation. All rights reserved.
+   * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+   * Released under the MIT license
+   * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+   *-----------------------------------------------------------------------------*)
+*/
