13ed350157685f8e4d38de2093bfc8ad1781470d
Alexandre de Souza <alexandre@aledsz.com.br>
2024-02-23 13:57:05 -0300

    Apply review comments

--- lib/livebook_web/helpers/html_helpers.ex
+++ lib/livebook_web/helpers/html_helpers.ex
@@ -36,7 +36,7 @@ defmodule LivebookWeb.HTMLHelpers do
     name
     |> String.trim()
     |> String.downcase()
-    |> String.replace(~r/[\p{P}\p{S}]/, "")
+    |> String.replace(~r/[^\s\w]/u, "")
     |> String.replace(~r/\s+/u, "-")
     |> case do
       "" -> Base.url_encode64(name, padding: false)
