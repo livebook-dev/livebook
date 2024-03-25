5b288a8f04b51ea6c39df305a38c6723f5ab0442
José Valim <jose.valim@dashbit.co>
2024-03-25 22:24:32 +0100

    Update lib/livebook/teams/requests.ex

--- lib/livebook/teams/requests.ex
+++ lib/livebook/teams/requests.ex
@@ -305,7 +305,7 @@ defmodule Livebook.Teams.Requests do
 
   defp add_team_auth(req, team) do
     if team.offline do
-      Req.merge(adapter: fn req ->
+      Req.merge(req, adapter: fn req ->
         {req, Req.Response.new(status: 401)}
       end)
     else
