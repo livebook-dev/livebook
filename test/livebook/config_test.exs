2611873c664d39a40b9d6bd4999258b6c7dfae8b
Ed Bond <celpa.firl@gmail.com>
2024-03-27 17:16:01 -0500

    updating based upon review

--- test/livebook/config_test.exs
+++ test/livebook/config_test.exs
@@ -44,6 +44,19 @@ defmodule Livebook.ConfigTest do
                  {:custom, LivebookWeb.SessionIdentity, "extra"}
       end)
     end
+
+    test "ZTA provider setting tests" do
+      assert {:custom, Livebook.ZTA.GlobalTest, "123"} ==
+               Livebook.Config.identity_provider!(
+                 "TEST_IDENTITY_custom:Livebook.ZTA.GlobalTest:123"
+               )
+
+      assert {:zta, Livebook.ZTA.Cloudflare, "123"} ==
+               Livebook.Config.identity_provider!("TEST_IDENTITY_cloudflare:123")
+
+      assert {:session, LivebookWeb.SessionIdentity, :unused} ==
+               Livebook.Config.identity_provider!("EMPTY")
+    end
   end
 
   defp with_env(env_vars, fun) do
