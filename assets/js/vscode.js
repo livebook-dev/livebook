5ce5b8bde69d32285732068d765eda64dad7078f
Jonas Grünwald <code@jonasg.me>
2024-04-02 12:37:50 +0200

    add fix for copy+paste within vscode iframe

--- assets/js/vscode.js
+++ assets/js/vscode.js
@@ -1,9 +1,20 @@
 export function registerVsCodeEventHandlers() {
-  /**
-   * Send keydown events to the parent window to allow for vscode keybindings to work
-   * See: https://github.com/microsoft/vscode/issues/65452#issuecomment-586036474
-   */
   window.document.addEventListener('keydown', e => {
+    /**
+     * Workaround for copy / paste not working within vscode iframe on mac
+     * See: https://github.com/microsoft/vscode/issues/129178#issuecomment-1674169530
+     */
+    if ((e.ctrlKey || e.metaKey) && e.code === "KeyC") {
+      document.execCommand("copy");
+    } else if ((e.ctrlKey || e.metaKey) && e.code === "KeyX") {
+      document.execCommand("cut");
+    } else if ((e.ctrlKey || e.metaKey) && eevent.code === "KeyV") {
+      document.execCommand("paste");
+    }
+    /**
+     * Send keydown events to the parent window to allow for vscode keybindings to work
+     * See: https://github.com/microsoft/vscode/issues/65452#issuecomment-586036474
+     */
     window.parent.postMessage(JSON.stringify({
       type: e.type,
       altKey: e.altKey,
