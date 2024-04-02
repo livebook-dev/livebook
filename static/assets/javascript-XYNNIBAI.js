83abf93be8f802fa47712e9b9813045b1be85baf
Jonas Grünwald <code@jonasg.me>
2024-04-02 16:43:16 +0200

    rebase onto last release version

---
+++ static/assets/javascript-XYNNIBAI.js
@@ -0,0 +1,11 @@
+import{a as t,b as e,c as i}from"./chunk-BKBTJF2S.js";import"./chunk-LBWD3LM5.js";import{e as s}from"./chunk-EP6THQJ3.js";var a,n,o=s(()=>{i();a=t,n={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer}});o();export{a as conf,n as language};
+/*! Bundled license information:
+
+monaco-editor/esm/vs/basic-languages/javascript/javascript.js:
+  (*!-----------------------------------------------------------------------------
+   * Copyright (c) Microsoft Corporation. All rights reserved.
+   * Version: 0.43.0(94c055bcbdd49f04a0fa15515e848542a79fb948)
+   * Released under the MIT license
+   * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+   *-----------------------------------------------------------------------------*)
+*/
---
+++ static/assets/typescript-WNOR4IJM.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-BKBTJF2S.js";import"./chunk-LBWD3LM5.js";import"./chunk-EP6THQJ3.js";c();export{a as conf,b as language};
