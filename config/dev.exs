1cac107402bf9c2d9fe34460a39be095d6791d44
nixxquality <nixx@is-fantabulo.us>
2024-04-08 13:58:45 +0200

    Replace dev assets watcher with cross-platform one (#2553)

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,8 @@ config :livebook, LivebookWeb.Endpoint,
   debug_errors: true,
   check_origin: false,
   watchers: [
-    npm: ["run", "watch", cd: Path.expand("../assets", __DIR__)]
+    # We invoke node rather than an npm task, so that it works on Windows
+    node: ["build.js", "--watch", cd: Path.expand("../assets", __DIR__)]
   ]
 
 config :livebook,
