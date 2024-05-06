1f75e3450d9597efafb0e3a4fa8b81c2a12d04ec
José Valim <jose.valim@dashbit.co>
2024-05-06 18:15:37 +0200

    Desktop changes

--- lib/livebook/application.ex
+++ lib/livebook/application.ex
@@ -132,7 +132,7 @@ defmodule Livebook.Application do
       :ok
     else
       Livebook.Config.abort!("""
-      You must specify ERL_AFLAGS=\"-epmd_module 'Elixir.Livebook.EPMD' -start_epmd false -erl_epmd_port 0\" with LIVEBOOK_EPMDLESS. \
+      You must specify ERL_AFLAGS=\"-epmd_module Elixir.Livebook.EPMD -start_epmd false -erl_epmd_port 0\" with LIVEBOOK_EPMDLESS. \
       The epmd module can be found inside #{Application.app_dir(:livebook, "priv/ebin")}.
       """)
     end
