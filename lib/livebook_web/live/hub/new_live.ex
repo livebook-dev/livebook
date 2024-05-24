55274f811776247c9fd0d6436a341fa1e30bfdd6
Hugo Baraúna <hugo.barauna@gmail.com>
2024-05-24 14:55:23 -0300

    Updates copy about Teams (#2619)

--- lib/livebook_web/live/hub/new_live.ex
+++ lib/livebook_web/live/hub/new_live.ex
@@ -51,7 +51,7 @@ defmodule LivebookWeb.Hub.NewLive do
               target="_blank"
               phx-no-format
             >
-              Livebook Teams</a> amplifies Livebook with features designed for teams and businesses. It is currently in closed beta.
+              Livebook Teams</a> enables you to deploy internal tools built with Elixir and Livebook to your own infrastructure. It is currently in closed beta.
           </p>
           <p class="mt-4 text-gray-700">
             To create a Teams organization, you must <a
