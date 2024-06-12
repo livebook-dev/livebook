f8064128c33e2848d2651994e465f509c7e5dac9
Alexandre de Souza <alexandre@aledsz.com.br>
2024-06-12 11:50:33 -0300

    Use enum values that only Livebook knows

--- proto/lib/livebook_proto/app_deployment_status_type.pb.ex
+++ proto/lib/livebook_proto/app_deployment_status_type.pb.ex
@@ -1,8 +1,6 @@
 defmodule LivebookProto.AppDeploymentStatusType do
   use Protobuf, enum: true, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
 
-  field :connecting, 0
-  field :preparing, 1
-  field :available, 2
-  field :deactivated, 4
+  field :preparing, 0
+  field :available, 1
 end
