6f00ce9bb42049e6bb050146943c3d983c318326
Hugo Baraúna <hugo.barauna@gmail.com>
2024-12-06 09:36:01 -0300

    Fix: user payload was always nil

--- test/livebook/users/user_test.exs
+++ test/livebook/users/user_test.exs
@@ -6,12 +6,13 @@ defmodule Livebook.Users.UserTest do
   describe "change/2" do
     test "given valid attributes returns and updated user" do
       user = build(:user)
-      attrs = %{"name" => "Jake Peralta", "hex_color" => "#000000"}
+      attrs = %{"name" => "Jake Peralta", "hex_color" => "#000000", "payload" => %{"country" => "US"}}
       changeset = User.changeset(user, attrs)
 
       assert changeset.valid?
       assert get_field(changeset, :name) == "Jake Peralta"
       assert get_field(changeset, :hex_color) == "#000000"
+      assert get_field(changeset, :payload) == %{"country" => "US"}
     end
 
     test "given invalid color returns an error" do
