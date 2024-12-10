eedb0a7e465c14dec14e306c38a8739045a286c5
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-12-10 22:25:56 +0800

    Include date in logs

--- config/config.exs
+++ config/config.exs
@@ -11,7 +11,7 @@ config :livebook, LivebookWeb.Endpoint,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
+  format: "$date $time $metadata[$level] $message\n",
   metadata: [:request_id]
 
 # Use Jason for JSON parsing in Phoenix
