7bd5af9a45ca9812913b530fab18add9e76cbcd8
Hugo Baraúna <hugo.barauna@gmail.com>
2025-01-29 16:25:11 -0300

    First draft of OIDC SSO docs

--- mix.exs
+++ mix.exs
@@ -256,6 +256,7 @@ defmodule Livebook.MixProject do
       "docs/teams/intro_to_teams.md",
       "docs/teams/shared_secrets.md",
       "docs/teams/shared_file_storages.md",
+      {"docs/teams/oidc_sso.md", title: "OIDC SSO"},
       "docs/authentication/basic_auth.md",
       "docs/authentication/cloudflare.md",
       "docs/authentication/google_iap.md",
