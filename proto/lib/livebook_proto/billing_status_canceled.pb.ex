46be61992d138a96c081fa3eae6d1a210cc596b5
Wojtek Mach <wojtek@wojtekmach.pl>
2025-02-11 14:05:34 +0100

    Update canceled status

--- proto/lib/livebook_proto/billing_status_canceled.pb.ex
+++ proto/lib/livebook_proto/billing_status_canceled.pb.ex
@@ -1,5 +1,3 @@
 defmodule LivebookProto.BillingStatusCanceled do
   use Protobuf, protoc_gen_elixir_version: "0.13.0", syntax: :proto3
-
-  field :cancel_at, 1, type: :int64, json_name: "cancelAt"
 end
