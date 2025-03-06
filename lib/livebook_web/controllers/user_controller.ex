c21bad21cd349bede57083077cfe47f899594048
Alexandre de Souza <alexandre@aledsz.com.br>
2025-03-06 14:55:29 -0300

    Fix issue with logging out from ZTA (#2953)

--- lib/livebook_web/controllers/user_controller.ex
+++ lib/livebook_web/controllers/user_controller.ex
@@ -3,12 +3,29 @@ defmodule LivebookWeb.UserController do
 
   def logout(conn, _params) do
     if get_session(conn, :user_id) do
-      conn
-      |> configure_session(renew: true)
-      |> clear_session()
-      |> render("logout.html")
+      if Livebook.Config.logout_enabled?() do
+        do_zta_logout(conn)
+      else
+        do_logout(conn)
+      end
     else
       redirect(conn, to: ~p"/")
+    end
+  end
+
+  defp do_logout(conn) do
+    conn
+    |> configure_session(renew: true)
+    |> clear_session()
+    |> render("logout.html")
+  end
+
+  defp do_zta_logout(conn) do
+    {_type, module, _key} = Livebook.Config.identity_provider()
+
+    case module.logout(LivebookWeb.ZTA, conn) do
+      :ok -> do_logout(conn)
+      {:error, reason} -> conn |> redirect(to: ~p"/") |> put_flash(:error, reason)
     end
   end
 end
