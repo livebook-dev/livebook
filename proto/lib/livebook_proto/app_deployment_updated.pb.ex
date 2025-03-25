e52ed86aee6e2c38de8b2b950a876d629f36a3f3
Alexandre de Souza <alexandre@aledsz.com.br>
2025-03-25 11:09:46 -0300

    Add `AppDeploymentUpdated` protobuf message (#2971)

---
+++ proto/lib/livebook_proto/app_deployment_updated.pb.ex
@@ -0,0 +1,5 @@
+defmodule LivebookProto.AppDeploymentUpdated do
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :app_deployment, 1, type: LivebookProto.AppDeployment, json_name: "appDeployment"
+end
