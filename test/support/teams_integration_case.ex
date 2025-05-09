da4b367ccb593ce6fb3f7d0f740300052676661a
Alexandre de Souza <alexandre@aledsz.com.br>
2025-05-09 11:32:49 -0300

    Authenticate user to test with Teams auth/authz

--- test/support/teams_integration_case.ex
+++ test/support/teams_integration_case.ex
@@ -1,7 +1,10 @@
 defmodule Livebook.TeamsIntegrationCase do
   use ExUnit.CaseTemplate
 
+  import Phoenix.ConnTest
   alias Livebook.TeamsServer
+
+  @endpoint LivebookWeb.Endpoint
 
   using do
     quote do
@@ -13,6 +16,7 @@ defmodule Livebook.TeamsIntegrationCase do
       alias Livebook.TeamsServer
 
       import Livebook.HubHelpers
+      import Livebook.TeamsIntegrationCase
     end
   end
 
@@ -30,5 +34,27 @@ defmodule Livebook.TeamsIntegrationCase do
     Application.put_env(:livebook, :teams_url, url, persistent: true)
 
     {:ok, node: node, token: token, user: user}
+  end
+
+  def authenticate_user_on_teams(conn, node, team) do
+    response =
+      conn
+      |> LivebookWeb.ConnCase.with_authorization(team.id)
+      |> get("/")
+      |> html_response(200)
+
+    [_, location] = Regex.run(~r/URL\("(.*?)"\)/, response)
+    uri = URI.parse(location)
+    %{"code" => code} = URI.decode_query(uri.query)
+
+    Livebook.HubHelpers.erpc_call(node, :allow_auth_request, [code])
+
+    session =
+      conn
+      |> LivebookWeb.ConnCase.with_authorization(team.id)
+      |> get("/", %{teams_identity: "", code: code})
+      |> Plug.Conn.get_session()
+
+    {Plug.Test.init_test_session(conn, session), code}
   end
 end
