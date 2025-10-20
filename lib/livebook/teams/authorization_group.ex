7dd368df7bf74eb470239bf50fce7bf22144f10b
Alexandre de Souza <alexandre@aledsz.com.br>
2025-10-20 18:47:04 -0300

    Implement new fields for app folder id

--- lib/livebook/teams/authorization_group.ex
+++ lib/livebook/teams/authorization_group.ex
@@ -3,12 +3,14 @@ defmodule Livebook.Teams.AuthorizationGroup do
 
   @type t :: %__MODULE__{
           provider_id: String.t() | nil,
-          group_name: String.t() | nil
+          group_name: String.t() | nil,
+          app_folder_ids: list(String.t())
         }
 
   @primary_key false
   embedded_schema do
     field :provider_id, :string
     field :group_name, :string
+    field :app_folder_ids, {:array, :string}
   end
 end
