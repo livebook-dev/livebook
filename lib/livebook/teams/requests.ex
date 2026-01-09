74756c2d227b70a6412673c93986f3432db79a02
Alexandre de Souza <alexandre@aledsz.com.br>
2026-01-09 17:44:16 -0300

    Support extra parameters to deploy an app from cli

--- lib/livebook/teams/requests.ex
+++ lib/livebook/teams/requests.ex
@@ -241,19 +241,24 @@ defmodule Livebook.Teams.Requests do
   @doc """
   Send a request to Livebook Team API to deploy an app using an org token.
   """
-  @spec deploy_app_from_cli(Team.t(), Teams.AppDeployment.t(), integer()) :: api_result()
-  def deploy_app_from_cli(team, app_deployment, deployment_group_id) do
+  @spec deploy_app_from_cli(Team.t(), Teams.AppDeployment.t(), integer(), map()) ::
+          api_result()
+  def deploy_app_from_cli(team, app_deployment, deployment_group_id, extra_params) do
     secret_key = Teams.derive_key(team.teams_key)
 
-    params = %{
-      title: app_deployment.title,
-      slug: app_deployment.slug,
-      multi_session: app_deployment.multi_session,
-      access_type: app_deployment.access_type,
-      app_folder_id: app_deployment.app_folder_id,
-      deployment_group_id: deployment_group_id,
-      sha: app_deployment.sha
-    }
+    params =
+      Map.merge(
+        extra_params,
+        %{
+          title: app_deployment.title,
+          slug: app_deployment.slug,
+          multi_session: app_deployment.multi_session,
+          access_type: app_deployment.access_type,
+          app_folder_id: app_deployment.app_folder_id,
+          deployment_group_id: deployment_group_id,
+          sha: app_deployment.sha
+        }
+      )
 
     encrypted_content = Teams.encrypt(app_deployment.file, secret_key)
     upload("/api/v1/cli/org/apps", encrypted_content, params, team)
