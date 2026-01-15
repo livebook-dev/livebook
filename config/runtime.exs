10ff0429e2617322e52db29705c7b4e2d3566f75
Hugo Baraúna <hugo.barauna@gmail.com>
2026-01-15 16:34:36 -0300

    Config for Tidewave client URL (#3118)
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,2 +1,8 @@
 import Config
 Livebook.config_runtime()
+
+if config_env() == :dev do
+  if tidewave_client_url = System.get_env("TIDEWAVE_CLIENT_URL") do
+    config :tidewave, client_url: tidewave_client_url
+  end
+end
