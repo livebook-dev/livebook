bb27702cfc7c6dffef3a2582d08c1d2812d8dd7a
Jonatan Kłosko <jonatanklosko@gmail.com>
2026-03-03 22:24:26 +0100

    Fix installing escript from GitHub and installing livebook as a dependency

--- lib/mix/tasks/compile.ensure_livebook_priv.ex
+++ lib/mix/tasks/compile.ensure_livebook_priv.ex
@@ -5,17 +5,29 @@ defmodule Mix.Tasks.Compile.EnsureLivebookPriv do
 
   @impl true
   def run(_args) do
+    # Livebook may be installed as a depepdency. This is the case in
+    # nerves_livebook, but also when using escript.install. In those
+    # cases assets.setup is not called, so we use this custom compiler
+    # to invoke the assets build and make those work transparently.
+    #
+    # Note that this is not relevant for escript.install from Hex,
+    # because we publish the package with priv/ already built and
+    # without the source assets. However, it is relevant when
+    # installing escript from GitHub.
+
     # Use an absolute path, instead of relying on the current mix
-    # project, so the task can be invoked by nerves_livebook.
+    # project, since livebook may be a dependency.
     project_dir = Path.expand("../../..", __DIR__)
     priv_static_dir = Path.join(project_dir, "priv/static")
 
-    # In case Livebook is used as a library as in nerves_livebook,
-    # the assets.setup is not called, so we use this custom compiler
-    # to invoke the task and make the assets work transparently.
-    #
-    # This is also used when installing escript from GitHub.
     if Mix.env() == :prod and not File.exists?(priv_static_dir) do
+      # If livebook is a dependency, it doesn't get it's own deps/
+      # dir, instead it is a sibling in the deps folder. We rely on
+      # deps/ dir being available in assets/package.json. To make it
+      # work, we symlink the deps dir. If the deps dir is present,
+      # this simply fails.
+      _ = File.ln_s(Mix.Project.deps_path(), Path.join(project_dir, "deps"))
+
       Mix.shell().info("Generating priv/static")
 
       Mix.Task.run("bun.install", ~w"--if-missing")
