8f1eba8a6009221092f17d1d4b4e4db16cab1008
Wojtek Mach <wojtek@wojtekmach.pl>
2026-04-10 14:40:59 +0200

    Remove elixirkit_next

--- elixirkit_next/examples/tauri_project/.formatter.exs
+++
@@ -1,5 +0,0 @@
-[
-  import_deps: [:phoenix],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
-]
--- elixirkit_next/examples/tauri_project/test/example_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExampleWeb.PageControllerTest do
-  use ExampleWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
